6a80c83a3bf63b666741ba64a7db96eea2fd64c1
Ian Woodard <ianwoodard2000@gmail.com>
2022-08-10 19:33:40 -0700

    Adding add positional modal prototype

--- src/components/lend/modal/AddPositionModal.tsx
+++ src/components/lend/modal/AddPositionModal.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { TokenData } from '../../../data/TokenData';
+import { formatUSDAuto } from '../../../util/Numbers';
 import { FilledStylizedButton } from '../../common/Buttons';
 import {
   CloseableModal,
   DashedDivider,
-  HorizontalDivider,
   LABEL_TEXT_COLOR,
   VALUE_TEXT_COLOR,
 } from '../../common/Modal';
+import TokenAmountInput from '../../common/TokenAmountInput';
 import { Text } from '../../common/Typography';
 import { MODAL_BLACK_TEXT_COLOR } from '../../pool/PoolInteractionTabs';
 
 export type AddPositionModalProps = {
+  token: TokenData;
   open: boolean;
   setOpen: (open: boolean) => void;
   onConfirm: () => void;
@@ -19,11 +22,16 @@ export type AddPositionModalProps = {
 
 export default function AddPositionModal(props: AddPositionModalProps) {
   const {
+    token,
     open,
     setOpen,
     onConfirm,
     onCancel,
   } = props;
+
+  const [amount, setAmount] = React.useState('');
+  const pricePerToken = 1;
+
   return (
     <CloseableModal
       open={open}
@@ -31,67 +39,26 @@ export default function AddPositionModal(props: AddPositionModalProps) {
       onClose={onCancel}
       title='Add Position'
     >
-      <div className='flex justify-between items-center mb-4'>
+      <div className='mb-4'>
+        <TokenAmountInput
+          onChange={(updatedAmount: string) => {
+            setAmount(updatedAmount);
+          }}
+          value={amount}
+          tokenLabel={token?.ticker || ''}
+          max={'10'}
+          maxed={amount === '10'}
+        />
+      </div>
+      <div className='flex justify-between items-center mb-8'>
         <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>
-          Estimated Total
+          Estimated Value
         </Text>
         <DashedDivider />
         <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>
-          {/* {estimatedTotal} */}
+          {formatUSDAuto(pricePerToken * parseFloat(amount) || 0)}
         </Text>
       </div>
-      {/* <TokenBreakdown
-        token0Ticker={token0Ticker}
-        token1Ticker={token1Ticker}
-        token0Estimate={token0Estimate}
-        token1Estimate={token1Estimate}
-      /> */}
-      <HorizontalDivider />
-      <div className='flex flex-col gap-y-4 mb-8'>
-        <div className='flex justify-between items-center'>
-          <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>
-            Pool Selected
-          </Text>
-          <DashedDivider />
-          <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>
-            {/* {token0Ticker} - {token1Ticker} */}
-          </Text>
-        </div>
-        <div className='flex justify-between items-center'>
-          <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>
-            Estimated Shares
-          </Text>
-          <DashedDivider />
-          <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>
-            {/* {numberOfShares} Shares */}
-          </Text>
-        </div>
-        <div className='flex justify-between items-center'>
-          <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>
-            Max Slippage
-          </Text>
-          <DashedDivider />
-          <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>
-            {/* {maxSlippage}% */}
-          </Text>
-        </div>
-        {/*Hidden for the time being*/}
-        <div className='hidden flex justify-between items-center'>
-          <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>
-            Network fee
-          </Text>
-          <DashedDivider />
-          <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>
-            {/* {networkFee}% */}
-          </Text>
-        </div>
-      </div>
-      {/* <HorizontalDivider />
-      <div className='flex justify-between items-center mb-8'>
-        <Text size='S' weight='medium' color={LABEL_TEXT_COLOR}>Network Fees</Text>
-        <DashedDivider />
-        <Text size='L' weight='medium' color={VALUE_TEXT_COLOR}>{networkFee} WETH</Text>
-      </div> */}
       <FilledStylizedButton
         size='M'
         fillWidth={true}
