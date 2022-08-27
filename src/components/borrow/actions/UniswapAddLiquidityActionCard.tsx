05512887d109758a6941f80da2fedfe1a4baa1bb
Ian Woodard <ianwoodard2000@gmail.com>
2022-08-27 10:57:17 -0500

    Cleaning up the liquidity chart

--- src/components/borrow/actions/UniswapAddLiquidityActionCard.tsx
+++ src/components/borrow/actions/UniswapAddLiquidityActionCard.tsx
@@ -6,110 +6,92 @@ import { BaseActionCard } from '../BaseActionCard';
 import { ActionCardProps, ActionProviders } from '../../../data/Actions';
 import { GetTokenData } from '../../../data/TokenData';
 import SteppedInput from '../LiquidityChartRangeInput/SteppedInput';
-import LiquidityChart, { ChartEntry } from '../LiquidityChartRangeInput/LiquidityChart';
+import LiquidityChart, {
+  ChartEntry,
+} from '../LiquidityChartRangeInput/LiquidityChart';
 import { theGraphUniswapV3Client } from '../../../App';
 import { UniswapTicksQuery } from '../../../util/GraphQL';
 import { ApolloQueryResult } from '@apollo/react-hooks';
 import Big from 'big.js';
 
 type UniswapV3GraphQLTick = {
-  tickIdx: string,
-  liquidityNet: string,
-  price0: string,
-  price1: string,
-  __typename: string,
+  tickIdx: string;
+  liquidityNet: string;
+  price0: string;
+  price1: string;
+  __typename: string;
 };
 
 type UniswapV3GraphQLTicksQueryResponse = {
   pools: {
-    token0: { decimals: string },
-    token1: { decimals: string },
-    liquidity: string,
-    tick: string,
-    ticks: UniswapV3GraphQLTick[],
-    __typename: string
-  }[],
-}
+    token0: { decimals: string };
+    token1: { decimals: string };
+    liquidity: string;
+    tick: string;
+    ticks: UniswapV3GraphQLTick[];
+    __typename: string;
+  }[];
+};
 
 export type TickData = {
-  tick: number,
-  liquidity: Big,
-  amount0: number,
-  amount1: number,
-  price1In0: number,
-  price0In1: number,
-  totalValueIn0: number,
-}
+  tick: number;
+  liquidity: Big;
+  amount0: number;
+  amount1: number;
+  price1In0: number;
+  price0In1: number;
+  totalValueIn0: number;
+};
 
-export const UNISWAP_V3_PAIRS = [
-  {
-    name: 'USDC/WETH',
-    token0: GetTokenData('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'),
-    token1: GetTokenData('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'),
-  },
-  {
-    name: 'WBTC/WETH',
-    token0: GetTokenData('0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'),
-    token1: GetTokenData('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'),
-  },
-];
+const ONE = new Big('1.0');
 
-function calcuateLiquidity(index: number, total: number) {
-  const distanceToMiddle = Math.abs(index - total / 2);
-  const distanceFromMiddle = total - distanceToMiddle;
-  return (
-    Math.random() * distanceFromMiddle * distanceFromMiddle * distanceFromMiddle
-  );
-}
-
-const ONE = new Big("1.0");
-let fakeData: Array<ChartEntry> = [];
-
-for (let i = 0; i < 250; i++) {
-  fakeData.push({
-    index: i,
-    isCurrent: i === 125,
-    activeLiquidity: calcuateLiquidity(i, 250),
-    price0: i * 0.1,
-    price1: Math.round((i * 8 + 1000 + Math.random()) * 100) / 100,
-  });
-}
-
-function calculateNearestPrice(price: number, data: ChartEntry[]) {
-  return data.reduce((prev: ChartEntry, cur: ChartEntry) => {
-    const prevDifference = Math.abs(prev.price1 - price);
-    const curDifference = Math.abs(cur.price1 - price);
+//TODO: make type for price index data
+function calculateNearestPrice(price: number, data: TickData[]) {
+  const nearest: TickData = data.reduce((prev: TickData, cur: TickData) => {
+    const prevDifference = Math.abs(prev.price1In0 - price);
+    const curDifference = Math.abs(cur.price1In0 - price);
     return curDifference < prevDifference ? cur : prev;
   });
+  return {
+    price: nearest.price1In0,
+    index: data.indexOf(nearest),
+  }
 }
 
 export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
   const { token0, token1, previousActionCardState, onChange, onRemove } = props;
   const [lower, setLower] = useState({
-    price: fakeData[Math.floor(fakeData.length / 4)].price1.toString(),
-    index: Math.floor(fakeData.length / 4),
+    price: '',
+    index: 0,
   });
   const [upper, setUpper] = useState({
-    price: fakeData[Math.floor(fakeData.length / 4) + Math.floor(fakeData.length / 2)].price1.toString(),
-    index: Math.floor(fakeData.length / 4) + Math.floor(fakeData.length / 2),
+    price: '',
+    index: 0,
   });
   const [chartData, setChartData] = useState<TickData[]>([]);
 
   useEffect(() => {
     let mounted = true;
-    async function fetch(poolAddress: string, minTick: number, maxTick: number, tickSpacing: number) {
-      const uniswapV3GraphQLTicksQueryResponse = await theGraphUniswapV3Client.query({
-        query: UniswapTicksQuery,
-        variables: {
-          poolAddress: poolAddress,
-          minTick: minTick,
-          maxTick: maxTick,
-        },
-      }) as ApolloQueryResult<UniswapV3GraphQLTicksQueryResponse>;
+    async function fetch(
+      poolAddress: string,
+      minTick: number,
+      maxTick: number,
+      tickSpacing: number
+    ) {
+      const uniswapV3GraphQLTicksQueryResponse =
+        (await theGraphUniswapV3Client.query({
+          query: UniswapTicksQuery,
+          variables: {
+            poolAddress: poolAddress,
+            minTick: minTick,
+            maxTick: maxTick,
+          },
+        })) as ApolloQueryResult<UniswapV3GraphQLTicksQueryResponse>;
 
       if (mounted) {
         if (!uniswapV3GraphQLTicksQueryResponse.data.pools) return;
-        const poolLiquidityData = uniswapV3GraphQLTicksQueryResponse.data.pools[0];
+        const poolLiquidityData =
+          uniswapV3GraphQLTicksQueryResponse.data.pools[0];
 
         const token0Decimals = Number(poolLiquidityData.token0.decimals);
         const token1Decimals = Number(poolLiquidityData.token1.decimals);
@@ -122,7 +104,7 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
         const tickDataLeft: TickData[] = [];
         const tickDataRight: TickData[] = [];
 
-        console.log("Current Tick:");
+        console.log('Current Tick:');
         console.log(currentTick);
 
         // MARK -- filling out data for ticks *above* the current tick
@@ -143,7 +125,10 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
 
           const sqrtPL = price0.sqrt();
           const sqrtPU = price0.mul(new Big(1.0001).pow(tickSpacing)).sqrt();
-          const amount0 = liquidity.mul(ONE.div(sqrtPL).minus(ONE.div(sqrtPU))).div(10 ** token0Decimals).toNumber();
+          const amount0 = liquidity
+            .mul(ONE.div(sqrtPL).minus(ONE.div(sqrtPU)))
+            .div(10 ** token0Decimals)
+            .toNumber();
 
           tickDataRight.push({
             tick,
@@ -170,7 +155,10 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
 
           const sqrtPL = price0.sqrt();
           const sqrtPU = price0.mul(new Big(1.0001).pow(tickSpacing)).sqrt();
-          const amount1 = liquidity.mul(sqrtPU.minus(sqrtPL)).div(10 ** token1Decimals).toNumber();
+          const amount1 = liquidity
+            .mul(sqrtPU.minus(sqrtPL))
+            .div(10 ** token1Decimals)
+            .toNumber();
 
           if (i === splitIdx - 1) {
             console.log(tick);
@@ -189,16 +177,39 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
           });
         }
 
-        const tickData = tickDataLeft.reverse().concat(...tickDataRight);
-        console.log(tickData);
-        setChartData(tickData);
+        //TODO: temporarily splicing data
+        const tickData = tickDataLeft
+          .reverse()
+          .concat(...tickDataRight)
+          .splice(500);
+        const reversedTickData = [...tickData].reverse();
+        setChartData(reversedTickData);
+        if (reversedTickData.length > 0) {
+          setLower({
+            price:
+              reversedTickData[
+                Math.floor(reversedTickData.length / 4)
+              ].price1In0.toString(),
+            index: Math.floor(reversedTickData.length / 4),
+          });
+          setUpper({
+            price:
+              reversedTickData[
+                Math.floor(reversedTickData.length / 4) +
+                  Math.floor(reversedTickData.length / 2)
+              ].price1In0.toString(),
+            index:
+              Math.floor(reversedTickData.length / 4) +
+              Math.floor(reversedTickData.length / 2),
+          });
+        }
       }
     }
 
-    fetch("0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", 200000, 210000, 10);
+    fetch('0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640', 200000, 210000, 10);
     return () => {
       mounted = false;
-    }
+    };
   }, []);
 
   return (
@@ -207,13 +218,13 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
       actionProvider={ActionProviders.UniswapV3}
       onRemove={onRemove}
     >
-      {chartData.length > 0 &&
-        (<LiquidityChart
-          data={chartData.reverse()}
+      {chartData.length > 0 && (
+        <LiquidityChart
+          data={chartData}
           rangeStart={lower.index}
           rangeEnd={upper.index}
-        />)
-      }
+        />
+      )}
       <div className='flex flex-row gap-2'>
         <SteppedInput
           value={lower.price}
@@ -221,10 +232,10 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
           token0={token0}
           token1={token1}
           onChange={(value) => {
-            const nearest = calculateNearestPrice(parseInt(value), fakeData);
-            if (nearest.index < upper.index) { 
+            const nearest = calculateNearestPrice(parseInt(value), chartData);
+            if (nearest.index < upper.index) {
               setLower({
-                price: nearest.price1.toString(),
+                price: nearest.price.toString(),
                 index: nearest.index,
               });
             }
@@ -232,24 +243,24 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
           onDecrement={() => {
             if (lower.index > 0) {
               setLower({
-                price: fakeData[lower.index - 1].price1.toString(),
+                price: chartData[lower.index - 1].price1In0.toString(),
                 index: lower.index - 1,
-              })
+              });
             }
           }}
           onIncrement={() => {
             if (lower.index + 1 < upper.index) {
               setLower({
-                price: fakeData[lower.index + 1].price1.toString(),
+                price: chartData[lower.index + 1].price1In0.toString(),
                 index: lower.index + 1,
-              })
+              });
             }
           }}
-          decrementDisabled={
-            lower.price === '' || lower.index <= 0
-          }
+          decrementDisabled={lower.price === '' || lower.index <= 0}
           incrementDisabled={
-            lower.price === '' || lower.index + 1 >= upper.index || lower.index + 1 === fakeData.length
+            lower.price === '' ||
+            lower.index + 1 >= upper.index ||
+            lower.index + 1 === chartData.length
           }
         />
         <SteppedInput
@@ -258,10 +269,10 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
           token0={token0}
           token1={token1}
           onChange={(value) => {
-            const nearest = calculateNearestPrice(parseInt(value), fakeData);
+            const nearest = calculateNearestPrice(parseInt(value), chartData);
             if (nearest.index > lower.index) {
               setUpper({
-                price: nearest.price1.toString(),
+                price: nearest.price.toString(),
                 index: nearest.index,
               });
             }
@@ -269,23 +280,23 @@ export default function UniswapAddLiquidityActionCard(props: ActionCardProps) {
           onDecrement={() => {
             if (upper.index - 1 > 0 && upper.index - 1 > lower.index) {
               setUpper({
-                price: fakeData[upper.index - 1].price1.toString(),
+                price: chartData[upper.index - 1].price1In0.toString(),
                 index: upper.index - 1,
-              })
+              });
             }
           }}
           onIncrement={() => {
-            if (upper.index < fakeData.length) {
+            if (upper.index < chartData.length) {
               setUpper({
-                price: fakeData[upper.index + 1].price1.toString(),
+                price: chartData[upper.index + 1].price1In0.toString(),
                 index: upper.index + 1,
-              })
+              });
             }
           }}
-          // decrementDisabled={
-          //   amount0 === '' || parseFloat(amount1) <= decrementStep
-          // }
-          // incrementDisabled={amount1 === ''}
+          decrementDisabled={
+            upper.price === '' || upper.index - 1 <= lower.index
+          }
+          incrementDisabled={upper.price === '' || upper.index + 1 >= chartData.length}
         />
       </div>
     </BaseActionCard>
