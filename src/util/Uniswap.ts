f81471feaf72bc84739dd589357fc5e8166352c9
Ian Woodard <ianwoodard2000@gmail.com>
2022-08-29 12:10:06 -0500

    Adding getCurrentTick helper function

---
+++ src/util/Uniswap.ts
@@ -0,0 +1,9 @@
+import { ethers } from 'ethers';
+
+/**
+ * 
+ * @returns the current tick for a given Uniswap pool
+ */
+export async function getCurrentTick(/* Add what you need */): Promise<number> {
+  return 205000;
+}
