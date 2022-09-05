1769a96957d779d217cae3fd39da38acab499114
Ian Woodard <ianwoodard2000@gmail.com>
2022-09-05 18:16:09 -0400

    Adding confirm button

--- src/pages/BorrowActionsPage.tsx
+++ src/pages/BorrowActionsPage.tsx
@@ -435,7 +435,6 @@ export default function BorrowActionsPage() {
             />
           </div>
         </div>
-        
       </BodyWrapper>
       <FullscreenModal
           open={actionModalOpen}
