650c31f50f37b5a76307f4108c516421bbb7e0b0
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2022-09-06 13:53:49 -0500

    Simplify MarginAccountActions args getters

--- src/connector/MarginAccountActions.ts
+++ src/connector/MarginAccountActions.ts
@@ -3,111 +3,125 @@ import { BigNumber, Contract, ContractReceipt, Signer, ethers } from 'ethers';
 
 import BlendPoolAbi from '../assets/abis/AloeBlend.json';
 import Big from 'big.js';
+import JSBI from 'jsbi';
 import { BlendPoolStats } from '../data/BlendPoolDataResolver';
 
 import MarginAccountAbi from '../assets/abis/MarginAccount.json';
 import { ActionCardState, ActionID } from '../data/Actions';
 import { TokenData } from '../data/TokenData';
 
-export async function modifyMarginAccount(
-    actionCardResults: ActionCardState[],
-    token0: TokenData,
-    token1: TokenData,
-    kitty0Address: string,
-    kitty1Address: string,
-    signer: Signer,
-) {
-    const actions: number[] = [];
-    const args: string[] = [];
+function getTransferInActionArgs(token: TokenData, amount: number): string {
+  const address = token.address;
+  const bigAmount = new Big(amount.toFixed(Math.min(token.decimals, 20))).mul(10 ** token.decimals);
 
-    for (const result of actionCardResults) {
-        let address: string;
-        let amount: Big;
+  return ethers.utils.defaultAbiCoder.encode(['address', 'uint256'], [address, bigAmount.toFixed(0)]);
+}
 
-        switch (result.actionId) {
-            case ActionID.TRANSFER_IN:
-                if (!result.aloeResult) continue;
+function getTransferOutActionArgs(token: TokenData, amount: number): string {
+  const address = token.address;
+  const bigAmount = new Big(amount.toFixed(Math.min(token.decimals, 20))).mul(10 ** token.decimals);
 
-                if (result.aloeResult.token0RawDelta) {
-                    address = token0.address;
-                    amount = new Big(result.aloeResult.token0RawDelta.toFixed(6)).mul(10 ** token0.decimals);
-                } else if (result.aloeResult.token1RawDelta) {
-                    address = token1.address;
-                    amount = new Big(result.aloeResult.token1RawDelta.toFixed(6)).mul(10 ** token1.decimals);
-                } else if (result.aloeResult.token0PlusDelta) {
-                    address = kitty0Address;
-                    amount = new Big(result.aloeResult.token0PlusDelta.toFixed(6)).mul(10 ** 18);
-                } else {
-                    address = kitty1Address;
-                    amount = new Big(result.aloeResult.token1PlusDelta!.toFixed(6)).mul(10 ** 18);
-                }
+  return ethers.utils.defaultAbiCoder.encode(['address', 'uint256'], [address, bigAmount.toFixed(0)]);
+}
 
-                actions.push(result.actionId);
-                args.push(ethers.utils.defaultAbiCoder.encode(
-                    ["address", "uint256"],
-                    [address, amount.toFixed(0)]
-                ));
-                break;
+function getMintActionArgs(kitty: TokenData, amount: number): string {
+  const address = kitty.address;
+  const bigAmount = new Big(amount.toFixed(Math.min(kitty.decimals, 20))).mul(10 ** 18);
 
-            case ActionID.TRANSFER_OUT:
-                if (!result.aloeResult) continue;
+  return ethers.utils.defaultAbiCoder.encode(['address', 'uint256'], [address, bigAmount.toFixed(0)]);
+}
 
-                if (result.aloeResult.token0RawDelta) {
-                    address = token0.address;
-                    amount = new Big((-result.aloeResult.token0RawDelta).toFixed(6)).mul(10 ** token0.decimals);
-                } else if (result.aloeResult.token1RawDelta) {
-                    address = token1.address;
-                    amount = new Big((-result.aloeResult.token1RawDelta).toFixed(6)).mul(10 ** token1.decimals);
-                } else if (result.aloeResult.token0PlusDelta) {
-                    address = kitty0Address;
-                    amount = new Big((-result.aloeResult.token0PlusDelta).toFixed(6)).mul(10 ** 18);
-                } else {
-                    address = kitty1Address;
-                    amount = new Big((-result.aloeResult.token1PlusDelta!).toFixed(6)).mul(10 ** 18);
-                }
+function getBurnActionArgs(kitty: TokenData, amount: number): string {
+  const address = kitty.address;
+  const bigAmount = new Big(amount.toFixed(Math.min(kitty.decimals, 20))).mul(10 ** 18);
 
-                actions.push(result.actionId);
-                args.push(ethers.utils.defaultAbiCoder.encode(
-                    ["address", "uint256"],
-                    [address, amount.toFixed(0)]
-                ));
-                break;
-            
-            case ActionID.MINT:
-                if (!result.aloeResult) continue;
+  return ethers.utils.defaultAbiCoder.encode(['address', 'uint256'], [address, bigAmount.toFixed(0)]);
+}
 
-                if (result.aloeResult.token0PlusDelta) {
-                    address = kitty0Address;
-                    amount = new Big(result.aloeResult.token0PlusDelta.toFixed(6)).mul(10 ** 18);
-                } else {
-                    address = kitty1Address;
-                    amount = new Big(result.aloeResult.token1PlusDelta!.toFixed(6)).mul(10 ** 18);
-                }
+function getBorrowActionArgs(token0: TokenData, amount0: number, token1: TokenData, amount1: number): string {
+  const bigAmount0 = new Big(amount0.toFixed(Math.min(token0.decimals, 20))).mul(10 ** token0.decimals);
+  const bigAmount1 = new Big(amount1.toFixed(Math.min(token1.decimals, 20))).mul(10 ** token1.decimals);
 
-                actions.push(result.actionId);
-                args.push(ethers.utils.defaultAbiCoder.encode(
-                    ["address", "uint256"],
-                    [address, amount.toFixed(0)]
-                ));
-                break
+  return ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256'], [bigAmount0.toFixed(0), bigAmount1.toFixed(0)]);
+}
 
-            case ActionID.BURN:
-                if (!result.aloeResult) continue;
+function getRepayActionArgs(token0: TokenData, amount0: number, token1: TokenData, amount1: number): string {
+  const bigAmount0 = new Big(amount0.toFixed(Math.min(token0.decimals, 20))).mul(10 ** token0.decimals);
+  const bigAmount1 = new Big(amount1.toFixed(Math.min(token1.decimals, 20))).mul(10 ** token1.decimals);
 
-                if (result.aloeResult.token0PlusDelta) {
-                    address = kitty0Address;
-                    amount = new Big((-result.aloeResult.token0PlusDelta).toFixed(6)).mul(10 ** 18);
-                } else {
-                    address = kitty1Address;
-                    amount = new Big((-result.aloeResult.token1PlusDelta!).toFixed(6)).mul(10 ** 18);
-                }
+  return ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256'], [bigAmount0.toFixed(0), bigAmount1.toFixed(0)]);
+}
 
-                actions.push(result.actionId);
-                args.push(ethers.utils.defaultAbiCoder.encode(
-                    ["address", "uint256"],
-                    [address, amount.toFixed(0)]
-                ));
-                break
+function getAddLiquidityActionArgs(lower: number, upper: number, liquidity: JSBI): string {
+    return ethers.utils.defaultAbiCoder.encode(['int24', 'int24', 'uint128'], [lower, upper, liquidity.toString(10)]);
+}
+
+export function getActionArgsFor(
+  state: ActionCardState,
+  token0: TokenData,
+  token1: TokenData,
+  kitty0: TokenData,
+  kitty1: TokenData
+): string | null {
+  switch (state.actionId) {
+    case ActionID.TRANSFER_IN:
+      if (state.aloeResult?.token0RawDelta) return getTransferInActionArgs(token0, state.aloeResult.token0RawDelta);
+      if (state.aloeResult?.token1RawDelta) return getTransferInActionArgs(token1, state.aloeResult.token1RawDelta);
+      if (state.aloeResult?.token0PlusDelta) return getTransferInActionArgs(kitty0, state.aloeResult.token0PlusDelta);
+      if (state.aloeResult?.token1PlusDelta) return getTransferInActionArgs(kitty1, state.aloeResult.token1PlusDelta);
+      return null;
+
+    case ActionID.TRANSFER_OUT:
+      if (state.aloeResult?.token0RawDelta) return getTransferOutActionArgs(token0, -state.aloeResult.token0RawDelta);
+      if (state.aloeResult?.token1RawDelta) return getTransferOutActionArgs(token1, -state.aloeResult.token1RawDelta);
+      if (state.aloeResult?.token0PlusDelta) return getTransferOutActionArgs(kitty0, -state.aloeResult.token0PlusDelta);
+      if (state.aloeResult?.token1PlusDelta) return getTransferOutActionArgs(kitty1, -state.aloeResult.token1PlusDelta);
+      return null;
+
+    case ActionID.MINT:
+      if (state.aloeResult?.token0PlusDelta) return getMintActionArgs(kitty0, state.aloeResult.token0PlusDelta);
+      if (state.aloeResult?.token1PlusDelta) return getMintActionArgs(kitty1, state.aloeResult.token1PlusDelta);
+      return null;
+
+    case ActionID.BURN:
+      if (state.aloeResult?.token0PlusDelta) return getBurnActionArgs(kitty0, -state.aloeResult.token0PlusDelta);
+      if (state.aloeResult?.token1PlusDelta) return getBurnActionArgs(kitty1, -state.aloeResult.token1PlusDelta);
+      return null;
+
+    case ActionID.BORROW:
+      if (state.aloeResult?.token0DebtDelta)
+        return getBorrowActionArgs(kitty0, state.aloeResult.token0DebtDelta, kitty1, 0);
+      if (state.aloeResult?.token1DebtDelta)
+        return getBorrowActionArgs(kitty0, 0, kitty1, state.aloeResult.token1DebtDelta);
+      return null;
+
+    case ActionID.REPAY:
+      if (state.aloeResult?.token0DebtDelta)
+        return getRepayActionArgs(kitty0, -state.aloeResult.token0DebtDelta, kitty1, 0);
+      if (state.aloeResult?.token1DebtDelta)
+        return getRepayActionArgs(kitty0, 0, kitty1, -state.aloeResult.token1DebtDelta);
+      return null;
+
+    case ActionID.ADD_LIQUIDITY:
+        if (state.uniswapResult?.uniswapPosition) {
+            const {lowerBound, upperBound, liquidity} = state.uniswapResult.uniswapPosition;
+            if (lowerBound === null || upperBound === null) return null;
+
+            return getAddLiquidityActionArgs(
+                Math.min(lowerBound, upperBound),
+                Math.max(lowerBound, upperBound),
+                liquidity
+            );
         }
-    }
+        return null;
+
+    case ActionID.REMOVE_LIQUIDITY:
+        return null;
+
+    case ActionID.SWAP:
+        return null;
+
+    default:
+        return null
+  }
 }
