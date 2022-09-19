843cd86340fae3f727f7eacb3440c26cd84dfd2b
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2022-09-19 01:27:20 -0500

    Restore existing code

---
+++ src/components/borrow/modal/SuccessfulTxnModal.tsx
@@ -0,0 +1,41 @@
+import SuccessIcon from '../../../assets/svg/success.svg';
+import { FilledStylizedButton } from '../../common/Buttons';
+import {
+  CloseableModal,
+  HorizontalDivider,
+} from '../../common/Modal';
+import { MODAL_BLACK_TEXT_COLOR } from '../../common/Modal';
+
+export type SuccessfulTxnModalProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  onConfirm: () => void;
+};
+
+export default function SuccessfulTxnModal(props: SuccessfulTxnModalProps) {
+  const {
+    open,
+    setOpen,
+    onConfirm,
+  } = props;
+  return (
+    <CloseableModal open={open} setOpen={setOpen} onClose={onConfirm} title='Transaction Successful'>
+      <div className='flex justify-center items-center'>
+        <img src={SuccessIcon} alt='success' />
+      </div>
+      <HorizontalDivider />
+      <FilledStylizedButton
+        size='M'
+        fillWidth={true}
+        color={MODAL_BLACK_TEXT_COLOR}
+        className='mt-8'
+        onClick={() => {
+          setOpen(false);
+          onConfirm();
+        }}
+      >
+        Okay!
+      </FilledStylizedButton>
+    </CloseableModal>
+  );
+}
---
+++ src/components/common/AppBody.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export default styled.div`
+  ${tw`w-full h-full text-white flex flex-col min-h-screen py-16`}
+  font-family: 'Satoshi-Variable';
+  font-weight: 500;
+`;
