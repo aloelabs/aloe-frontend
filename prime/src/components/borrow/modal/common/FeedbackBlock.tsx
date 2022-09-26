a9f7a2a6a9d24a1ed1e32d7c7aefceebbd5c6019
Adam Egyed <adamegyed@gmail.com>
2022-09-26 13:03:15 -0700

    [Prime] Add feedback form link to transaction confirmation modals (success, failure) and footer (#73)

---
+++ prime/src/components/borrow/modal/common/FeedbackBlock.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { MESSAGE_TEXT_COLOR } from '../../../common/Modal';
+import { Text } from '../../../common/Typography';
+
+export default function FeedbackBlock() {
+  return (
+    <Text size='M' weight='medium' color={MESSAGE_TEXT_COLOR}>
+        We'd love to hear your feedback! If you have a second, please{' '}
+        <a
+        href={'https://coda.io/form/Aloe-II-Web-App-Feedback-Form_dtlDhQRDThF'}
+        target='_blank'
+        className='underline'
+        rel='noopener noreferrer'
+        title='Share feedback!'
+        >share your experience</a>!
+      </Text>
+  );
+}
