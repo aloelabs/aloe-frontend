2bab7f235b7eb0621cb9db3b2b0f4d831cf6cba5
Aneesh Atri <aneesh.atri@gmail.com>
2022-09-29 09:25:57 -0400

    Factoring out AppBody component

--- prime/src/App.tsx
+++ prime/src/App.tsx
@@ -5,7 +5,7 @@ import WagmiProvider from './connector/WagmiProvider';
 import Header from './components/header/Header';
 import Footer from './components/common/Footer';
 
-import AppBody from './components/common/AppBody';
+import AppBody from 'shared';
 import { RedirectPartialPath } from './util/RedirectPartialPath';
 import ScrollToTop from './util/ScrollToTop';
 import { IS_DEV } from './util/Env';
--- prime/src/components/common/AppBody.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import tw from 'twin.macro';
-
-export default styled.div`
-  ${tw`w-full h-full text-white flex flex-col min-h-screen py-16`}
-  font-family: 'Satoshi-Variable';
-  font-weight: 500;
-`;
