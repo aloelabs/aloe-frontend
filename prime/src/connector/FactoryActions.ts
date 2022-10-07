fe230f0aa819b3166ace1b00b89b4676743265be
Ian Woodard <ianwoodard2000@gmail.com>
2022-10-07 19:58:32 -0400

    Refactoring create margin account flow

--- prime/src/connector/FactoryActions.ts
+++ prime/src/connector/FactoryActions.ts
@@ -4,10 +4,19 @@ import { BigNumber, Contract, ContractReceipt, Signer } from 'ethers';
 import FactoryABI from '../assets/abis/Factory.json';
 import { ALOE_II_FACTORY_ADDRESS_BUILT_IN_FAUCET_GOERLI } from '../data/constants/Addresses';
 
+/**
+ *
+ * @param signer the signer to use for the transaction
+ * @param poolAddress the pool address to be used for the margin account
+ * @param ownerAddress the address of the owner of the margin account
+ * @param commencementCallback a callback to be called when the transaction is sent
+ * @param completionCallback a callback to be called when the transaction is completed
+ */
 export async function createMarginAccount(
   signer: Signer,
   poolAddress: string,
   ownerAddress: string,
+  commencementCallback: () => void,
   completionCallback: (receipt?: ContractReceipt) => void
 ): Promise<void> {
   // TODO: Temporarily replacing actual factory with one that has a built-in faucet upon MarginAccount creation
@@ -28,7 +37,10 @@ export async function createMarginAccount(
 
   try {
     const transactionResponse = await factory.createMarginAccount(poolAddress, ownerAddress, transactionOptions);
+    // The txn was approved, now we wait
+    commencementCallback();
     const receipt = await transactionResponse.wait(BLOCKS_TO_WAIT);
+    // Tada! The txn is complete
     completionCallback(receipt);
   } catch (e) {
     // User probably rejected in MetaMask or wallet
