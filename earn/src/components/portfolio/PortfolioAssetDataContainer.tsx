25362d6f81be2a8b7d312f016c4164550a9f15cb
Ian Woodard <ianwoodard2000@gmail.com>
2022-11-10 11:02:27 -0500

    Adding pie chart to portfolio grid

--- earn/src/components/portfolio/PortfolioAssetDataContainer.tsx
+++ earn/src/components/portfolio/PortfolioAssetDataContainer.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
 import { Display, Text } from 'shared/lib/components/common/Typography';
 
 import { TokenData } from '../../data/TokenData';
 import { TokenBalance } from '../../pages/PortfolioPage';
-import { formatTokenAmount, roundPercentage } from '../../util/Numbers';
+import { rgba } from '../../util/Colors';
+import { formatTokenAmountCompact, roundPercentage } from '../../util/Numbers';
 import { APYContainer, BalanceContainer, PieChartContainer } from './PortfolioGrid';
+import PortfolioPieChartWidget, { PortfolioPieChartSlice } from './PortfolioPieChartWidget';
 
 export type PortfolioAssetDataContainerProps = {
   balances: TokenBalance[];
@@ -12,32 +16,57 @@ export type PortfolioAssetDataContainerProps = {
 };
 
 export default function PortfolioAssetDataContainer(props: PortfolioAssetDataContainerProps) {
-  const { balances, activeAsset } = props;
+  const { balances, activeAsset, activeColor } = props;
+
+  const [activeIndex, setActiveIndex] = useState<number>(-1);
 
   const activeBalances = balances.filter(
     (balance) => activeAsset && balance.token.referenceAddress === activeAsset.referenceAddress
   );
   const totalTokenBalance = activeBalances.reduce((acc, balance) => acc + balance.balance, 0);
+  const activeSlices: PortfolioPieChartSlice[] = activeBalances.map((balance: TokenBalance, i: number) => {
+    return {
+      token: balance.token,
+      percent: balance.balance / totalTokenBalance || 0,
+      color: activeColor ? rgba(activeColor, (i + 1) / activeBalances.length) : 'transparent',
+      isKitty: balance.isKitty,
+      pairName: balance.pairName,
+      index: i,
+    };
+  });
+  const activeSlice = activeIndex >= 0 ? activeSlices[activeIndex] : null;
   let totalBalance = totalTokenBalance;
   let apySum = activeBalances.reduce((acc, balance) => acc + balance.apy * balance.balance, 0);
   let apy = apySum / totalBalance || 0;
+  if (activeSlice) {
+    totalBalance = activeBalances[activeIndex].balance;
+    apySum = activeBalances[activeIndex].apy;
+    apy = apySum;
+  }
   return (
     <>
-      <PieChartContainer>{/* TODO */}</PieChartContainer>
-      <BalanceContainer>
+      <PieChartContainer>
+        <PortfolioPieChartWidget
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+          slices={activeSlices}
+          token={activeAsset}
+        />
+      </PieChartContainer>
+      <BalanceContainer className={activeIndex !== -1 ? 'active' : ''}>
         <Text size='M' weight='bold' color='rgba(130, 160, 182, 1)'>
           Balance
         </Text>
         <div>
           <Display size='L' className='inline-block mr-0.5'>
-            {formatTokenAmount(totalBalance)}
+            {formatTokenAmountCompact(totalBalance, 3)}
           </Display>
           <Display size='S' className='inline-block ml-0.5'>
             {activeAsset?.ticker || ''}
           </Display>
         </div>
       </BalanceContainer>
-      <APYContainer>
+      <APYContainer className={activeIndex !== -1 ? 'active' : ''}>
         <Text size='M' weight='bold' color='rgba(130, 160, 182, 1)'>
           APY
         </Text>
