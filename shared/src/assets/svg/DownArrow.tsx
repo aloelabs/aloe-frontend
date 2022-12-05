cc95445dceb2d7cb9b024dc04e8b028782a0e546
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2022-12-05 01:12:15 -0500

    Fixing svg errors (#172)

--- shared/src/assets/svg/DownArrow.tsx
+++ shared/src/assets/svg/DownArrow.tsx
@@ -6,9 +6,9 @@ export default function DropdownArrowDown(props: SVGProps) {
       <path
         d='M3.3333 8.0705 8 12.7372 12.6666 8.0705M8 12.7372V3.4039'
         stroke='#82a0b6'
-        stroke-width='1.25'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.25'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
