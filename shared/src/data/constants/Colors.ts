c8f04bb843c0242f02ba9379afdcac7c3439fb5a
Ian Woodard <ianwoodard2000@gmail.com>
2022-12-07 00:42:05 -0500

    working on adding color constants

---
+++ shared/src/data/constants/Colors.ts
@@ -0,0 +1,24 @@
+export const GREY_100 = '#ECF7FF';
+export const GREY_200 = '#CCDFED';
+export const GREY_300 = '#82A0B6';
+export const GREY_400 = '#4B6980';
+export const GREY_500 = '#385265';
+export const GREY_600 = '#2B4050';
+export const GREY_700 = '#1A2934';
+export const GREY_800 = '#0D171E';
+export const GREY_900 = '#060B0F';
+
+export const HEADER_BG = GREY_900;
+export const MAIN_BG = '#070E12';
+export const CARD_BG = GREY_800;
+
+export const ACCENT_1 = '#D9ED92';
+export const ACCENT_2 = '#99D98C';
+export const ACCENT_3 = '#52B69A';
+
+export const PURPLE = '#52B69A';
+export const LIGHT_PURPLE = 'rgba(134, 94, 242, 0.25)';
+
+export const SUCCESS = '#00C143';
+export const ERROR = '#EB5757';
+export const WARNING = '#F2C94C';
