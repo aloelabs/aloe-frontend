7a3652d07c2536c4bf0cc1cdd44793231a597c96
Ian Woodard <ianwoodard2000@gmail.com>
2022-12-26 10:50:57 -0600

    Fixing amount to shares

--- earn/src/data/hooks/UseAmountToShares.ts
+++ earn/src/data/hooks/UseAmountToShares.ts
@@ -3,18 +3,17 @@ import { useEffect, useState } from 'react';
 import Big from 'big.js';
 import { useContractRead } from 'wagmi';
 
-import KittyLensABI from '../../assets/abis/KittyLens.json';
-import { ALOE_II_KITTY_LENS_ADDRESS } from '../constants/Addresses';
+import KittyABI from '../../assets/abis/Kitty.json';
 import { Kitty } from '../Kitty';
 import { Token } from '../Token';
 
 export function useAmountToShares(token: Token, kitty: Kitty, withdrawAmount: string) {
   const [state, setState] = useState<string | null>(null);
   const { data: amountOfShares } = useContractRead({
-    address: ALOE_II_KITTY_LENS_ADDRESS,
-    abi: KittyLensABI,
-    functionName: 'amountToShares',
-    args: [kitty.address, new Big(withdrawAmount || '0').mul(10 ** token.decimals).toFixed(0)] as const,
+    address: kitty.address,
+    abi: KittyABI,
+    functionName: 'convertToShares',
+    args: [new Big(withdrawAmount || '0').mul(10 ** token.decimals).toFixed(0)] as const,
     watch: true,
   });
   useEffect(() => {
