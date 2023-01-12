7dcb22abb2aeda7d151b9404c231001a36128191
Ian Woodard <ianwoodard2000@gmail.com>
2023-01-11 19:33:52 -0500

    Updating prime modals

--- prime/src/components/borrow/modal/CreatedMarginAccountModal.tsx
+++ prime/src/components/borrow/modal/CreatedMarginAccountModal.tsx
@@ -1,35 +1,40 @@
 import { FilledStylizedButton } from 'shared/lib/components/common/Buttons';
+import Modal, { LABEL_TEXT_COLOR } from 'shared/lib/components/common/Modal';
+import { Text } from 'shared/lib/components/common/Typography';
 
 import SuccessIcon from '../../../assets/svg/success.svg';
-import { CloseableModal, HorizontalDivider } from '../../common/Modal';
 import { MODAL_BLACK_TEXT_COLOR } from '../../common/Modal';
 
 export type CreatedMarginAccountModalProps = {
-  open: boolean;
-  setOpen: (open: boolean) => void;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
   onConfirm: () => void;
 };
 
 export default function CreatedMarginAccountModal(props: CreatedMarginAccountModalProps) {
-  const { open, setOpen, onConfirm } = props;
+  const { isOpen, setIsOpen, onConfirm } = props;
   return (
-    <CloseableModal open={open} setOpen={setOpen} title='Account Created'>
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title='Account Created'>
       <div className='flex justify-center items-center'>
         <img src={SuccessIcon} alt='success' />
       </div>
-      <HorizontalDivider />
+      <div className='w-full mt-4'>
+        <Text size='M' weight='bold' color={LABEL_TEXT_COLOR} className='text-center'>
+          Your margin account has been created and will be available momentarily.
+        </Text>
+      </div>
       <FilledStylizedButton
         size='M'
         fillWidth={true}
         color={MODAL_BLACK_TEXT_COLOR}
         className='mt-8'
         onClick={() => {
-          setOpen(false);
+          setIsOpen(false);
           onConfirm();
         }}
       >
         Sweet!
       </FilledStylizedButton>
-    </CloseableModal>
+    </Modal>
   );
 }
