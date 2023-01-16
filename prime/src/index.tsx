5a387efa3cbc42072317efbb79a0aa567a006d90
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-01-16 16:32:02 -0500

    Switching from hash router to browser router (#233)

--- prime/src/index.tsx
+++ prime/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import './index.css';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
