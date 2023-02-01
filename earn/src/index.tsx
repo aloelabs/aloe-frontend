d680d33f8760236c34d65d1046d380baf7bfab03
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-02-01 00:55:20 -0500

    Adding sentry (#271)

--- earn/src/index.tsx
+++ earn/src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+
+Sentry.init({
+  dsn: 'https://71630ca179ea4a89801fa13180e823cf@o4504602696024064.ingest.sentry.io/4504602711687168',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
