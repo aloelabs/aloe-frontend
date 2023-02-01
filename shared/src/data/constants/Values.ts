7e3507b1ddc06d2d5fe91c2ea12b86a09a5a8b54
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-02-01 18:13:29 -0500

    Updating default chain to optimism (#275)

--- shared/src/data/constants/Values.ts
+++ shared/src/data/constants/Values.ts
@@ -1,4 +1,4 @@
 import { chain } from 'wagmi';
 
-export const DEFAULT_CHAIN = chain.goerli;
+export const DEFAULT_CHAIN = chain.optimism;
 export const DEFAULT_ETHERSCAN_URL = 'https://etherscan.io';
