ddd1a9b99f932f554380be63873d2e6da8d613ff
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-02-01 22:46:59 -0600

    Add % to IV in earn (got lost in merges)

--- earn/src/components/portfolio/LendingPairPeerCard.tsx
+++ earn/src/components/portfolio/LendingPairPeerCard.tsx
@@ -251,7 +251,7 @@ export default function LendingPairPeerCard(props: LendingPairPeerCardProps) {
             <Text size='S' weight='bold' color='rgba(130, 160, 182, 1)'>
               IV
             </Text>
-            <Display size='L'>{roundPercentage(selectedLendingPair.iv, 1)}</Display>
+            <Display size='L'>{roundPercentage(selectedLendingPair.iv, 1)}%</Display>
           </SmallCardBodyItem>
         </CardBody>
       </Container>
