94e68cebbf07b9d796cb58efe2aa144c1713336b
ianwoodard <ianwoodard2000@gmail.com>
2023-02-16 10:10:09 -0500

    Removing redundant code

--- earn/src/pages/BorrowPage.tsx
+++ earn/src/pages/BorrowPage.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import AppPage from 'shared/lib/components/common/AppPage';
 import { Text } from 'shared/lib/components/common/Typography';
 import styled from 'styled-components';
-import { Address, useAccount, useContract, useProvider, useSigner } from 'wagmi';
+import { Address, useAccount, useContract, useProvider } from 'wagmi';
 
 import { ChainContext } from '../App';
 import KittyLensAbi from '../assets/abis/KittyLens.json';
