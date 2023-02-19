e25afc5a1cde2e26e1ee540291ae09003ccb28e9
ianwoodard <ianwoodard2000@gmail.com>
2023-02-18 22:27:41 -0500

    removing redundant castings

--- earn/src/util/Multicall.ts
+++ earn/src/util/Multicall.ts
@@ -4,6 +4,7 @@ import { BigNumber } from 'ethers';
 export function convertBigNumbers(callReturnContext: CallReturnContext[]): CallReturnContext[] {
   return callReturnContext.map((value) => {
     value.returnValues = value.returnValues.map((returnValue) => {
+      // If the return value is a BigNumber, convert it to an ethers BigNumber
       if (returnValue?.type === 'BigNumber' && returnValue?.hex) {
         returnValue = BigNumber.from(returnValue.hex);
       }
