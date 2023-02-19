8b3ecef93f30f594b0de4c1008eca40ebc9ec902
ianwoodard <ianwoodard2000@gmail.com>
2023-02-18 22:13:50 -0500

    Fixing optional

--- earn/src/util/Multicall.ts
+++ earn/src/util/Multicall.ts
@@ -4,8 +4,8 @@ import { BigNumber } from 'ethers';
 export function convertBigNumbers(callReturnContext: CallReturnContext[]): CallReturnContext[] {
   return callReturnContext.map((value) => {
     value.returnValues = value.returnValues.map((returnValue) => {
-      if (returnValue?.type === 'BigNumber') {
-        returnValue = BigNumber.from(returnValue?.hex);
+      if (returnValue?.type === 'BigNumber' && returnValue?.hex) {
+        returnValue = BigNumber.from(returnValue.hex);
       }
       return returnValue as any[];
     });
