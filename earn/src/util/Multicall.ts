53d838f8afb362475b94e4e919771c63b8740722
ianwoodard <ianwoodard2000@gmail.com>
2023-02-18 22:38:02 -0500

    renaming

--- earn/src/util/Multicall.ts
+++ earn/src/util/Multicall.ts
@@ -1,15 +1,15 @@
 import { CallReturnContext } from 'ethereum-multicall';
 import { BigNumber } from 'ethers';
 
-export function convertBigNumbers(callReturnContext: CallReturnContext[]): CallReturnContext[] {
-  return callReturnContext.map((value) => {
-    value.returnValues = value.returnValues.map((returnValue) => {
+export function convertBigNumbers(callReturnContexts: CallReturnContext[]): CallReturnContext[] {
+  return callReturnContexts.map((callReturnContext) => {
+    callReturnContext.returnValues = callReturnContext.returnValues.map((returnValue) => {
       // If the return value is a BigNumber, convert it to an ethers BigNumber
       if (returnValue?.type === 'BigNumber' && returnValue?.hex) {
         returnValue = BigNumber.from(returnValue.hex);
       }
       return returnValue;
     });
-    return value;
+    return callReturnContext;
   });
 }
