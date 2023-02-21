56654b8560b0651ddfb0fac9c48381c1f2a59a2f
ianwoodard <ianwoodard2000@gmail.com>
2023-02-21 16:30:47 -0500

    Adding mobile version of the navbar

--- shared/src/components/navbar/NavBar.tsx
+++ shared/src/components/navbar/NavBar.tsx
@@ -5,8 +5,9 @@ import { NavLink } from 'react-router-dom';
 import DiscordFooterIcon from '../../assets/svg/DiscordFooter';
 import MediumFooterIcon from '../../assets/svg/MediumFooter';
 import TwitterFooterIcon from '../../assets/svg/TwitterFooter';
+import CloseModal from '../../assets/svg/CloseModal';
+import MenuIcon from '../../assets/svg/Menu';
 import { Text } from '../common/Typography';
-import { RESPONSIVE_BREAKPOINT_SM } from '../../data/constants/Breakpoints';
 import styled from 'styled-components';
 import { Chain, useAccount, useNetwork, useDisconnect } from 'wagmi';
 
@@ -16,6 +17,13 @@ import EllipsisIcon from '../../assets/svg/Ellipsis';
 import AccountInfo from './AccountInfo';
 import ChainSelector from './ChainSelector';
 import ConnectWalletButton from './ConnectWalletButton';
+import {
+  RESPONSIVE_BREAKPOINTS,
+  RESPONSIVE_BREAKPOINT_MD,
+  RESPONSIVE_BREAKPOINT_XS,
+} from '../../data/constants/Breakpoints';
+import useMediaQuery from '../../data/hooks/UseMediaQuery';
+import useLockScroll from '../../data/hooks/UseLockScroll';
 
 const FOOTER_LINK_TEXT_COLOR = 'rgba(75, 105, 128, 1)';
 
@@ -23,7 +31,7 @@ const DesktopLogo = styled(AloeDesktopLogo)`
   width: 100px;
   height: 40px;
   margin-right: 32px;
-  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     display: none;
   }
 `;
@@ -32,7 +40,7 @@ const MobileLogo = styled(AloeMobileLogo)`
   width: 40px;
   height: 40px;
   margin-right: 32px;
-  @media (min-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (min-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     display: none;
   }
 `;
@@ -48,6 +56,27 @@ const DesktopTopNav = styled.div`
   padding: 0 32px;
 `;
 
+const TabletBottomNav = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 64px;
+  background-color: rgba(6, 11, 15, 1);
+  border-top: 1px solid rgba(26, 41, 52, 1);
+  padding: 8px 16px;
+
+  @media (min-width: ${RESPONSIVE_BREAKPOINT_MD}) {
+    display: none;
+  }
+
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_XS}) {
+    display: none;
+  }
+`;
+
 const MobileBottomNav = styled.div`
   position: fixed;
   display: flex;
@@ -60,7 +89,7 @@ const MobileBottomNav = styled.div`
   border-top: 1px solid rgba(26, 41, 52, 1);
   padding: 8px 16px;
 
-  @media (min-width: 768px) {
+  @media (min-width: ${RESPONSIVE_BREAKPOINT_XS}) {
     display: none;
   }
 `;
@@ -70,7 +99,7 @@ const VerticalDivider = styled.div`
   height: 64px;
   background-color: rgba(26, 41, 52, 1);
 
-  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     width: 100%;
     height: 1px;
   }
@@ -83,10 +112,29 @@ const FooterLink = styled(Text)`
   }
 `;
 
+const ExternalFooterLinkWithIcon = styled.a`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+
+  padding: 8px 16px;
+  color: ${FOOTER_LINK_TEXT_COLOR};
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+
+    svg {
+      path {
+        fill: rgba(255, 255, 255, 1);
+      }
+    }
+  }
+`;
+
 const DesktopNavLinks = styled.div`
   display: flex;
 
-  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     display: none;
   }
 `;
@@ -109,7 +157,7 @@ const DesktopNavLink = styled(NavLink)`
     border-bottom: 1px solid rgba(26, 41, 52, 1);
   }
 
-  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     width: 100%;
     padding: 12px 0px;
   }
@@ -133,7 +181,7 @@ const ExternalDesktopLink = styled.a`
     border-bottom: 1px solid rgba(26, 41, 52, 1);
   }
 
-  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
     width: 100%;
     padding: 12px 0px;
   }
@@ -184,6 +232,36 @@ const StyledPopoverPanel = styled(Popover.Panel)`
   padding: 16px;
 `;
 
+const NavOverlay = styled.div`
+  position: fixed;
+  top: 64px;
+  bottom: 64px;
+  left: 0;
+  right: 0;
+  background-color: rgb(13, 23, 30);
+  z-index: 40;
+  padding: 16px;
+  overflow-y: scroll;
+
+  @media (min-width: ${RESPONSIVE_BREAKPOINT_XS}) {
+    display: none;
+  }
+`;
+
+const MenuIconWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+  cursor: pointer;
+  user-select: none;
+
+  svg {
+    path {
+      stroke: #ffffff;
+    }
+  }
+`;
+
 export type NavBarLink = {
   label: string;
   to: string;
@@ -200,16 +278,27 @@ export type NavBarProps = {
 
 export function NavBar(props: NavBarProps) {
   const { links, isAllowedToInteract, activeChain, checkboxes, setActiveChain } = props;
+  const [isNavDrawerOpen, setIsNavDrawerOpen] = useState(false);
   const account = useAccount();
   const network = useNetwork();
   const { disconnect } = useDisconnect();
   const [isSelectChainDropdownOpen, setIsSelectChainDropdownOpen] = useState(false);
+  const { lockScroll, unlockScroll } = useLockScroll();
   useEffect(() => {
     // Close the chain selector dropdown when the chain changes
     setIsSelectChainDropdownOpen(false);
   }, [network.chain]);
 
   const isOffline = !account.isConnected && !account.isConnecting;
+
+  const isBiggerThanMobile = useMediaQuery(RESPONSIVE_BREAKPOINTS.XS);
+
+  useEffect(() => {
+    if (isNavDrawerOpen && isBiggerThanMobile) {
+      setIsNavDrawerOpen(false);
+      unlockScroll();
+    }
+  }, [isBiggerThanMobile, isNavDrawerOpen, unlockScroll]);
 
   return (
     <>
@@ -260,7 +349,7 @@ export function NavBar(props: NavBarProps) {
           )}
         </div>
       </DesktopTopNav>
-      <MobileBottomNav>
+      <TabletBottomNav>
         {props.links.map((link, index) => (
           <React.Fragment key={index}>
             {link.isExternal ? (
@@ -346,7 +435,126 @@ export function NavBar(props: NavBarProps) {
             </div>
           </StyledPopoverPanel>
         </Popover>
+      </TabletBottomNav>
+      <MobileBottomNav>
+        <MenuIconWrapper
+          onClick={() => {
+            if (isNavDrawerOpen) {
+              setIsNavDrawerOpen(false);
+              unlockScroll();
+            } else {
+              lockScroll();
+              setIsNavDrawerOpen(true);
+            }
+          }}
+        >
+          {isNavDrawerOpen ? <CloseModal width={28} height={28} /> : <MenuIcon width={28} height={28} />}
+        </MenuIconWrapper>
       </MobileBottomNav>
+      {isNavDrawerOpen && (
+        <NavOverlay>
+          <div className='flex flex-col justify-between h-full'>
+            <div className='flex flex-col gap-4'>
+              {props.links.map((link, index) => (
+                <React.Fragment key={index}>
+                  {link.isExternal ? (
+                    <MobileExternalLink href={link.to} target='_blank' rel='noopener noreferrer'>
+                      <Text size='XL' weight='bold'>
+                        {link.label}
+                      </Text>
+                    </MobileExternalLink>
+                  ) : (
+                    <MobileNavLink
+                      to={link.to}
+                      onClick={() => {
+                        setIsNavDrawerOpen(false);
+                        unlockScroll();
+                      }}
+                    >
+                      <Text size='XL' weight='bold'>
+                        {link.label}
+                      </Text>
+                    </MobileNavLink>
+                  )}
+                </React.Fragment>
+              ))}
+            </div>
+            <div className='mt-4'>
+              <div className='flex flex-col gap-2'>
+                <FooterLink
+                  as='a'
+                  size='M'
+                  weight='medium'
+                  color={FOOTER_LINK_TEXT_COLOR}
+                  href={'https://aloe.capital/'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Main site
+                </FooterLink>
+                <FooterLink
+                  as='a'
+                  size='M'
+                  weight='medium'
+                  color={FOOTER_LINK_TEXT_COLOR}
+                  href={'https://docs.aloe.capital/'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Docs
+                </FooterLink>
+                <FooterLink
+                  as='a'
+                  size='M'
+                  weight='medium'
+                  color={FOOTER_LINK_TEXT_COLOR}
+                  href={'/terms.pdf'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Terms
+                </FooterLink>
+              </div>
+              <div className='flex flex-col gap-2 mt-2'>
+                <ExternalFooterLinkWithIcon
+                  href={'https://discord.com/invite/gpt4sUv6sw'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  title='Join our Discord'
+                  className=''
+                >
+                  <DiscordFooterIcon width={14} height={11} />
+                  <Text size='M' weight='medium' color='unset'>
+                    Discord
+                  </Text>
+                </ExternalFooterLinkWithIcon>
+                <ExternalFooterLinkWithIcon
+                  href={'https://twitter.com/aloecapital'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  title='Follow us on Twitter'
+                >
+                  <TwitterFooterIcon width={15} height={11} />
+                  <Text size='M' weight='medium' color='unset'>
+                    Twitter
+                  </Text>
+                </ExternalFooterLinkWithIcon>
+                <ExternalFooterLinkWithIcon
+                  href={'https://aloelabs.medium.com'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  title='Connect with us on Medium'
+                >
+                  <MediumFooterIcon width={21} height={11} />
+                  <Text size='M' weight='medium' color='unset'>
+                    Medium
+                  </Text>
+                </ExternalFooterLinkWithIcon>
+              </div>
+            </div>
+          </div>
+        </NavOverlay>
+      )}
     </>
   );
 }
