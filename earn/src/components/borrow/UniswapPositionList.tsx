fe917ab1722081bf97966901d14f7aca2c5f5aad
ianwoodard <ianwoodard2000@gmail.com>
2023-03-03 12:34:40 -0700

    Adding uniswap positions to earn borrow page and added withdraw functionality

---
+++ earn/src/components/borrow/UniswapPositionList.tsx
@@ -0,0 +1,301 @@
+import { useState } from 'react';
+
+import { SendTransactionResult } from '@wagmi/core';
+import { FilledGreyButton } from 'shared/lib/components/common/Buttons';
+import { Display, Text } from 'shared/lib/components/common/Typography';
+import styled from 'styled-components';
+
+import { sqrtRatioToPrice, sqrtRatioToTick } from '../../data/BalanceSheet';
+import { MarginAccount } from '../../data/MarginAccount';
+import {
+  getAmountsForLiquidity,
+  tickToPrice,
+  UniswapNFTPosition,
+  UniswapNFTPositionEntry,
+  UniswapPosition,
+} from '../../data/Uniswap';
+import { formatTokenAmount, roundPercentage, truncateDecimals } from '../../util/Numbers';
+import TokenPairIcons from '../common/TokenPairIcons';
+import { WithdrawUniswapNFTModal } from './modal/WithdrawUniswapNFTModal';
+
+const ACCENT_COLOR = 'rgba(130, 160, 182, 1)';
+const IN_RANGE_COLOR = '#00C143';
+const OUT_OF_RANGE_COLOR = '#EB5757';
+const IN_RANGE_BACKGROUND_COLOR = 'rgba(0, 193, 67, 0.1)';
+const OUT_OF_RANGE_BACKGROUND_COLOR = 'rgba(235, 87, 87, 0.1)';
+
+type SelectedUniswapPosition = {
+  uniswapPosition: UniswapPosition;
+  withdrawableNFT: UniswapNFTPositionEntry;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const PositionList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 16px;
+  justify-content: space-between;
+`;
+
+const UniswapPositionCardContainer = styled.div`
+  width: 100%;
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const UniswapPositionCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  background-color: rgb(13, 23, 30);
+  border-radius: 8px;
+  padding: 16px;
+  width: 100%;
+`;
+
+const InRangeBadgeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  background-color: ${IN_RANGE_BACKGROUND_COLOR};
+  align-items: center;
+  width: fit-content;
+  padding: 4px 8px;
+  border-radius: 8px;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${IN_RANGE_COLOR};
+  }
+`;
+
+const OutOfRangeBadgeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+  background-color: ${OUT_OF_RANGE_BACKGROUND_COLOR};
+  width: fit-content;
+  padding: 4px 8px;
+  border-radius: 8px;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${OUT_OF_RANGE_COLOR};
+  }
+`;
+
+export function InRangeBadge() {
+  return (
+    <InRangeBadgeWrapper>
+      <Text size='S' color={IN_RANGE_COLOR}>
+        In Range
+      </Text>
+    </InRangeBadgeWrapper>
+  );
+}
+
+export function OutOfRangeBadge() {
+  return (
+    <OutOfRangeBadgeWrapper>
+      <Text size='S' color={OUT_OF_RANGE_COLOR}>
+        Out of Range
+      </Text>
+    </OutOfRangeBadgeWrapper>
+  );
+}
+
+type UniswapPositionCardProps = {
+  marginAccount: MarginAccount;
+  uniswapPosition?: UniswapPosition;
+  withdrawableUniswapNFTs: Map<number, UniswapNFTPosition>;
+  setSelectedUniswapPosition: (uniswapPosition: SelectedUniswapPosition | null) => void;
+  setPendingTxn: (pendingTxn: SendTransactionResult | null) => void;
+};
+
+function UniswapPositionCard(props: UniswapPositionCardProps) {
+  const { marginAccount, uniswapPosition, withdrawableUniswapNFTs, setSelectedUniswapPosition } = props;
+  const { sqrtPriceX96, token0, token1 } = marginAccount;
+
+  const minPrice = uniswapPosition
+    ? tickToPrice(uniswapPosition.lower, marginAccount.token0.decimals, marginAccount.token1.decimals, true)
+    : 0;
+
+  const maxPrice = uniswapPosition
+    ? tickToPrice(uniswapPosition.upper, marginAccount.token0.decimals, marginAccount.token1.decimals, true)
+    : 0;
+
+  const [amount0, amount1] = uniswapPosition
+    ? getAmountsForLiquidity(uniswapPosition, sqrtRatioToTick(sqrtPriceX96), token0.decimals, token1.decimals)
+    : [0, 0];
+
+  const token0PerToken1 = sqrtRatioToPrice(sqrtPriceX96, token0.decimals, token1.decimals);
+  const amount0InTermsOfToken1 = amount0 * token0PerToken1;
+  const totalValue = amount0InTermsOfToken1 + amount1;
+
+  const amount0Percent = (amount0InTermsOfToken1 / totalValue) * 100;
+  const amount1Percent = (amount1 / totalValue) * 100;
+
+  const currentTick = sqrtRatioToTick(sqrtPriceX96);
+
+  const isInRange = uniswapPosition && currentTick >= uniswapPosition.lower && currentTick <= uniswapPosition.upper;
+
+  const withdrawableNFT = Array.from(withdrawableUniswapNFTs.entries()).find(([_, position]) => {
+    return position.tickLower === uniswapPosition?.lower && position.tickUpper === uniswapPosition?.upper;
+  });
+
+  return (
+    <UniswapPositionCardWrapper>
+      {uniswapPosition ? (
+        <div className='flex flex-col gap-4'>
+          <div className='flex justify-center items-center'>
+            <TokenPairIcons
+              token0IconPath={marginAccount.token0.iconPath}
+              token1IconPath={marginAccount.token1.iconPath}
+              token0AltText={`${marginAccount.token0.ticker}'s icon`}
+              token1AltText={`${marginAccount.token1.ticker}'s icon`}
+            />
+          </div>
+          <div className='flex justify-between'>
+            <div className='text-left'>
+              <Display size='XS' color={ACCENT_COLOR}>
+                {roundPercentage(amount0Percent, 1)}%
+              </Display>
+              <Display size='S'>{truncateDecimals(amount0.toString(), 5)}</Display>
+              <Text size='XS'>{marginAccount.token0.ticker}</Text>
+            </div>
+            <div className='text-right'>
+              <Display size='XS' color={ACCENT_COLOR}>
+                {roundPercentage(amount1Percent, 1)}%
+              </Display>
+              <Display size='S'>{truncateDecimals(amount1.toString(), 5)}</Display>
+              <Text size='XS'>{marginAccount.token1.ticker}</Text>
+            </div>
+          </div>
+          <div className='flex justify-between'>
+            <div className='text-left'>
+              <Text size='S' color={ACCENT_COLOR}>
+                Min Price
+              </Text>
+              <Display size='S'>{formatTokenAmount(minPrice, 5)}</Display>
+              <Text size='XS'>
+                {marginAccount.token1.ticker} per {marginAccount.token0.ticker}
+              </Text>
+            </div>
+            <div className='text-right'>
+              <Text size='S' color={ACCENT_COLOR}>
+                Max Price
+              </Text>
+              <Display size='S'>{formatTokenAmount(maxPrice, 5)}</Display>
+              <Text size='XS'>
+                {marginAccount.token1.ticker} per {marginAccount.token0.ticker}
+              </Text>
+            </div>
+          </div>
+          <div className='flex justify-between'>
+            {isInRange ? <InRangeBadge /> : <OutOfRangeBadge />}
+            {withdrawableNFT && (
+              <FilledGreyButton
+                size='S'
+                disabled={!withdrawableNFT}
+                onClick={() => {
+                  setSelectedUniswapPosition({
+                    uniswapPosition: uniswapPosition,
+                    withdrawableNFT: withdrawableNFT,
+                  });
+                }}
+              >
+                Withdraw
+              </FilledGreyButton>
+            )}
+          </div>
+        </div>
+      ) : (
+        <Text size='S' color={ACCENT_COLOR} className='text-center'>
+          Empty
+        </Text>
+      )}
+    </UniswapPositionCardWrapper>
+  );
+}
+
+export type UniswapPositionListProps = {
+  marginAccount?: MarginAccount;
+  uniswapPositions: readonly UniswapPosition[];
+  withdrawableUniswapNFTs: Map<number, UniswapNFTPosition>;
+  setPendingTxn: (pendingTxn: SendTransactionResult | null) => void;
+};
+
+export function UniswapPositionList(props: UniswapPositionListProps) {
+  const { marginAccount, uniswapPositions, withdrawableUniswapNFTs, setPendingTxn } = props;
+  const [selectedUniswapPosition, setSelectedUniswapPosition] = useState<SelectedUniswapPosition | null>(null);
+
+  if (!marginAccount) return null;
+  return (
+    <>
+      <Container>
+        <Text size='M'>Uniswap Positions</Text>
+        <PositionList>
+          <UniswapPositionCardContainer>
+            <Text size='S'>Slot A</Text>
+            <UniswapPositionCard
+              marginAccount={marginAccount}
+              uniswapPosition={uniswapPositions.length > 0 ? uniswapPositions[0] : undefined}
+              withdrawableUniswapNFTs={withdrawableUniswapNFTs}
+              setSelectedUniswapPosition={setSelectedUniswapPosition}
+              setPendingTxn={props.setPendingTxn}
+            />
+          </UniswapPositionCardContainer>
+          <UniswapPositionCardContainer>
+            <Text size='S'>Slot B</Text>
+            <UniswapPositionCard
+              marginAccount={marginAccount}
+              uniswapPosition={uniswapPositions.length > 1 ? uniswapPositions[1] : undefined}
+              withdrawableUniswapNFTs={withdrawableUniswapNFTs}
+              setSelectedUniswapPosition={setSelectedUniswapPosition}
+              setPendingTxn={props.setPendingTxn}
+            />
+          </UniswapPositionCardContainer>
+          <UniswapPositionCardContainer>
+            <Text size='S'>Slot C</Text>
+            <UniswapPositionCard
+              marginAccount={marginAccount}
+              uniswapPosition={uniswapPositions.length > 2 ? uniswapPositions[2] : undefined}
+              withdrawableUniswapNFTs={withdrawableUniswapNFTs}
+              setSelectedUniswapPosition={setSelectedUniswapPosition}
+              setPendingTxn={props.setPendingTxn}
+            />
+          </UniswapPositionCardContainer>
+        </PositionList>
+      </Container>
+      {selectedUniswapPosition && (
+        <WithdrawUniswapNFTModal
+          isOpen={selectedUniswapPosition !== null}
+          marginAccount={marginAccount}
+          uniswapPosition={selectedUniswapPosition.uniswapPosition}
+          existingUniswapPositions={uniswapPositions}
+          uniswapNFTPosition={selectedUniswapPosition.withdrawableNFT}
+          setIsOpen={() => {
+            setSelectedUniswapPosition(null);
+          }}
+          setPendingTxn={setPendingTxn}
+        />
+      )}
+    </>
+  );
+}
---
+++ earn/src/components/borrow/modal/WithdrawUniswapNFTModal.tsx
@@ -0,0 +1,279 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { SendTransactionResult } from '@wagmi/core';
+import { ethers } from 'ethers';
+import { FilledStylizedButton } from 'shared/lib/components/common/Buttons';
+import Modal from 'shared/lib/components/common/Modal';
+import { Display, Text } from 'shared/lib/components/common/Typography';
+import styled from 'styled-components';
+import { useAccount, useContractWrite, usePrepareContractWrite } from 'wagmi';
+
+import { ChainContext } from '../../../App';
+import MarginAccountABI from '../../../assets/abis/MarginAccount.json';
+import { sqrtRatioToPrice, sqrtRatioToTick } from '../../../data/BalanceSheet';
+import { ALOE_II_UNISWAP_NFT_MANAGER_ADDRESS } from '../../../data/constants/Addresses';
+import { MarginAccount } from '../../../data/MarginAccount';
+import {
+  getAmountsForLiquidity,
+  tickToPrice,
+  UniswapNFTPositionEntry,
+  UniswapPosition,
+  zip,
+} from '../../../data/Uniswap';
+import { formatTokenAmount, roundPercentage, truncateDecimals } from '../../../util/Numbers';
+import TokenPairIcons from '../../common/TokenPairIcons';
+import { InRangeBadge, OutOfRangeBadge } from '../UniswapPositionList';
+
+const ACCENT_COLOR = 'rgba(130, 160, 182, 1)';
+const TERTIARY_COLOR = '#4b6980';
+
+const GAS_ESTIMATE_WIGGLE_ROOM = 110; // 10% wiggle room
+
+enum ConfirmButtonState {
+  APPROVE_NFT_MANAGER,
+  APPROVING,
+  PENDING,
+  LOADING,
+  READY,
+}
+
+function getConfirmButton(state: ConfirmButtonState): { text: string; enabled: boolean } {
+  switch (state) {
+    case ConfirmButtonState.APPROVE_NFT_MANAGER:
+      return { text: 'Approve', enabled: true };
+    case ConfirmButtonState.APPROVING:
+      return { text: 'Approving', enabled: false };
+    case ConfirmButtonState.PENDING:
+      return { text: 'Pending', enabled: false };
+    case ConfirmButtonState.LOADING:
+      return { text: 'Loading', enabled: false };
+    case ConfirmButtonState.READY:
+      return { text: 'Confirm', enabled: true };
+    default:
+      return { text: 'Confirm', enabled: false };
+  }
+}
+
+export const UniswapNFTPositionButtonWrapper = styled.button.attrs((props: { active: boolean }) => props)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 8px 16px;
+  gap: 8px;
+  border-radius: 8px;
+  opacity: ${(props) => (props.active ? 1 : 0.25)};
+  filter: ${(props) => (props.active ? 'none' : 'grayscale(100%)')};
+  cursor: pointer;
+  background-color: rgba(26, 41, 52, 1);
+
+  &:hover {
+    filter: none;
+    opacity: 1;
+  }
+`;
+
+type WithdrawUniswapNFTButtonProps = {
+  marginAccount: MarginAccount;
+  uniswapPosition: UniswapPosition;
+  existingUniswapPositions: readonly UniswapPosition[];
+  uniswapNFTPosition: UniswapNFTPositionEntry;
+  userAddress: string;
+  setIsOpen: (open: boolean) => void;
+  setPendingTxn: (result: SendTransactionResult | null) => void;
+};
+
+function WithdrawUniswapNFTButton(props: WithdrawUniswapNFTButtonProps) {
+  const { marginAccount, uniswapPosition, existingUniswapPositions, uniswapNFTPosition, setIsOpen, setPendingTxn } =
+    props;
+  const { activeChain } = useContext(ChainContext);
+
+  const [isPending, setIsPending] = useState(false);
+
+  const data = ethers.utils.defaultAbiCoder.encode(
+    ['uint256', 'int24', 'int24', 'int128', 'uint144'],
+    [
+      uniswapNFTPosition[0],
+      uniswapNFTPosition[1].tickLower,
+      uniswapNFTPosition[1].tickUpper,
+      `${uniswapPosition.liquidity.toString(10)}`,
+      zip(
+        existingUniswapPositions.filter((position) => {
+          return position.lower !== uniswapPosition.lower || position.upper !== uniswapPosition.upper;
+        })
+      ),
+    ]
+  );
+  const { config: contractWriteConfig } = usePrepareContractWrite({
+    address: marginAccount.address,
+    abi: MarginAccountABI,
+    functionName: 'modify',
+    args: [ALOE_II_UNISWAP_NFT_MANAGER_ADDRESS, data, [true, true]],
+    chainId: activeChain.id,
+  });
+  if (contractWriteConfig.request) {
+    contractWriteConfig.request.gasLimit = contractWriteConfig.request.gasLimit.mul(GAS_ESTIMATE_WIGGLE_ROOM).div(100);
+  }
+  const {
+    write: contractWrite,
+    data: contractData,
+    isSuccess: contractDidSucceed,
+    isLoading: contractIsLoading,
+  } = useContractWrite(contractWriteConfig);
+
+  useEffect(() => {
+    if (contractDidSucceed && contractData) {
+      setPendingTxn(contractData);
+      setIsPending(false);
+      setIsOpen(false);
+    } else if (!contractIsLoading && !contractDidSucceed) {
+      setIsPending(false);
+    }
+  }, [contractDidSucceed, contractData, contractIsLoading, setPendingTxn, setIsOpen]);
+
+  let confirmButtonState = ConfirmButtonState.READY;
+
+  if (isPending) {
+    confirmButtonState = ConfirmButtonState.PENDING;
+  } else if (contractWriteConfig && contractWriteConfig.request === undefined) {
+    confirmButtonState = ConfirmButtonState.LOADING;
+  }
+
+  const confirmButton = getConfirmButton(confirmButtonState);
+
+  return (
+    <FilledStylizedButton
+      size='M'
+      fillWidth={true}
+      disabled={!confirmButton.enabled}
+      onClick={() => {
+        if (confirmButtonState === ConfirmButtonState.READY) {
+          setIsPending(true);
+          contractWrite?.();
+        }
+      }}
+    >
+      {confirmButton.text}
+    </FilledStylizedButton>
+  );
+}
+
+export type WithdrawUniswapNFTModalProps = {
+  marginAccount: MarginAccount;
+  uniswapPosition: UniswapPosition;
+  existingUniswapPositions: readonly UniswapPosition[];
+  uniswapNFTPosition: UniswapNFTPositionEntry;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+  setPendingTxn: (pendingTxn: SendTransactionResult | null) => void;
+};
+
+export function WithdrawUniswapNFTModal(props: WithdrawUniswapNFTModalProps) {
+  const { marginAccount, uniswapPosition, uniswapNFTPosition, isOpen, setIsOpen, setPendingTxn } = props;
+
+  const { address: userAddress } = useAccount();
+
+  const { sqrtPriceX96, token0, token1 } = marginAccount;
+
+  const minPrice = uniswapPosition
+    ? tickToPrice(uniswapPosition.lower, marginAccount.token0.decimals, marginAccount.token1.decimals, true)
+    : 0;
+
+  const maxPrice = uniswapPosition
+    ? tickToPrice(uniswapPosition.upper, marginAccount.token0.decimals, marginAccount.token1.decimals, true)
+    : 0;
+
+  const [amount0, amount1] = uniswapPosition
+    ? getAmountsForLiquidity(uniswapPosition, sqrtRatioToTick(sqrtPriceX96), token0.decimals, token1.decimals)
+    : [0, 0];
+
+  const token0PerToken1 = sqrtRatioToPrice(sqrtPriceX96, token0.decimals, token1.decimals);
+  const amount0InTermsOfToken1 = amount0 * token0PerToken1;
+  const totalValue = amount0InTermsOfToken1 + amount1;
+
+  const amount0Percent = (amount0InTermsOfToken1 / totalValue) * 100;
+  const amount1Percent = (amount1 / totalValue) * 100;
+
+  const currentTick = sqrtRatioToTick(sqrtPriceX96);
+
+  const isInRange = uniswapPosition && currentTick >= uniswapPosition.lower && currentTick <= uniswapPosition.upper;
+
+  if (!userAddress) {
+    return null;
+  }
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title='Withdraw Uniswap NFT Position' maxWidth='340px'>
+      <div className='flex flex-col items-center justify-center gap-8 w-full mt-2'>
+        <div className='flex flex-col gap-1 w-full'>
+          <div className='w-full max-w-[300px] m-auto mb-4'>
+            <div className='flex flex-col items-center justify-center gap-4 '>
+              <div className='w-full flex justify-between'>
+                <TokenPairIcons
+                  token0IconPath={token0.iconPath}
+                  token1IconPath={token1.iconPath}
+                  token0AltText={`${token0.ticker}'s icon`}
+                  token1AltText={`${token1.ticker}'s icon`}
+                />
+                {isInRange ? <InRangeBadge /> : <OutOfRangeBadge />}
+              </div>
+              <div className='w-full flex justify-between'>
+                <div className='text-left'>
+                  <Display size='XS' color={ACCENT_COLOR}>
+                    {roundPercentage(amount0Percent, 1)}%
+                  </Display>
+                  <Display size='S'>{truncateDecimals(amount0.toString(), 5)}</Display>
+                  <Text size='XS'>{marginAccount.token0.ticker}</Text>
+                </div>
+                <div className='text-right'>
+                  <Display size='XS' color={ACCENT_COLOR}>
+                    {roundPercentage(amount1Percent, 1)}%
+                  </Display>
+                  <Display size='S'>{truncateDecimals(amount1.toString(), 5)}</Display>
+                  <Text size='XS'>{marginAccount.token1.ticker}</Text>
+                </div>
+              </div>
+              <div className='w-full flex justify-between'>
+                <div className='text-left'>
+                  <Text size='S' color={ACCENT_COLOR}>
+                    Min Price
+                  </Text>
+                  <Display size='S'>{formatTokenAmount(minPrice, 5)}</Display>
+                  <Text size='XS'>
+                    {marginAccount.token1.ticker} per {marginAccount.token0.ticker}
+                  </Text>
+                </div>
+                <div className='text-right'>
+                  <Text size='S' color={ACCENT_COLOR}>
+                    Max Price
+                  </Text>
+                  <Display size='S'>{formatTokenAmount(maxPrice, 5)}</Display>
+                  <Text size='XS'>
+                    {marginAccount.token1.ticker} per {marginAccount.token0.ticker}
+                  </Text>
+                </div>
+              </div>
+            </div>
+          </div>
+          <WithdrawUniswapNFTButton
+            marginAccount={marginAccount}
+            uniswapPosition={uniswapPosition}
+            existingUniswapPositions={props.existingUniswapPositions}
+            uniswapNFTPosition={uniswapNFTPosition}
+            userAddress={userAddress}
+            setIsOpen={setIsOpen}
+            setPendingTxn={setPendingTxn}
+          />
+          <Text size='XS' color={TERTIARY_COLOR} className='w-full mt-2'>
+            By using our service, you agree to our{' '}
+            <a href='/terms.pdf' className='underline' rel='noreferrer' target='_blank'>
+              Terms of Service
+            </a>{' '}
+            and acknowledge that you may lose your money. Aloe Labs is not responsible for any losses you may incur. It
+            is your duty to educate yourself and be aware of the risks.
+          </Text>
+        </div>
+      </div>
+    </Modal>
+  );
+}
