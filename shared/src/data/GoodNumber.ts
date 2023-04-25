2610f71905e14355cd0ba562141ac1f7f4bd502b
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-04-24 20:28:29 -0500

    Update GN to support Q numbers

--- shared/src/data/GoodNumber.ts
+++ shared/src/data/GoodNumber.ts
@@ -4,8 +4,13 @@ import JSBI from 'jsbi';
 
 import { formatTokenAmount, formatTokenAmountCompact } from '../util/Numbers';
 
-function scalerFor(decimals: number) {
-  return `1${'0'.repeat(decimals)}`;
+function scalerFor(base: 2 | 10, resolution: number) {
+  if (base === 2) {
+    if (resolution % 4 !== 0) throw new Error('Q number resolution must be a multiple of 4');
+    return BigNumber.from(`0x1${'0'.repeat(resolution / 4)}`).toString();
+  }
+
+  return `1${'0'.repeat(resolution)}`;
 }
 
 function isInteger(x: Big) {
@@ -26,7 +31,9 @@ export enum GNFormat {
 export class GN {
   private Int: BigConstructor;
 
-  private readonly decimals: number;
+  private readonly base: 2 | 10;
+
+  private readonly resolution: number;
 
   private readonly scaler: string;
 
@@ -36,12 +43,12 @@ export class GN {
    * Create a GN
    * @param int A string representation of the desired number. May be in normal or scientific notation.
    * Infinity, NaN and hexadecimal literal strings, e.g. '0xff', are not valid.
-   * @param decimals The maximum number of decimals to keep for operations involving division (div, sqrt, pow)
+   * @param resolution The maximum number of decimals to keep for operations involving division (div, sqrt, pow)
    */
-  private constructor(int: string, decimals: number) {
-    if (decimals !== Math.floor(decimals)) throw new Error('`decimals` must be a whole number');
-    else if (decimals < 0) throw new Error('`decimals` cannot be negative');
-    else if (decimals > 1e6) throw new Error('`decimals` cannot be larger than 1000000');
+  private constructor(int: string, resolution: number, base: 2 | 10) {
+    if (resolution !== Math.floor(resolution)) throw new Error('`resolution` must be a whole number');
+    else if (resolution < 0) throw new Error('`resolution` cannot be negative');
+    else if (resolution > 1e6) throw new Error('`resolution` cannot be larger than 1000000');
 
     this.Int = Big();
     this.Int.DP = 0;
@@ -50,8 +57,9 @@ export class GN {
     this.Int.RM = Big.roundDown;
     this.Int.strict = true;
 
-    this.decimals = decimals;
-    this.scaler = scalerFor(this.decimals);
+    this.base = base;
+    this.resolution = resolution;
+    this.scaler = scalerFor(this.base, this.resolution);
     this.int = new this.Int(int);
 
     if (!isInteger(this.int)) {
@@ -59,9 +67,13 @@ export class GN {
     }
   }
 
+  private approxDP() {
+    return Math.ceil(this.resolution * Math.log10(this.base));
+  }
+
   private x() {
     const Decimal = Big();
-    Decimal.DP = this.decimals;
+    Decimal.DP = this.approxDP();
     Decimal.RM = Big.roundDown;
 
     return new Decimal(this.int.toFixed(0)).div(this.scaler);
@@ -72,43 +84,43 @@ export class GN {
   //////////////////////////////////////////////////////////////*/
 
   cmp(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in cmp`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in cmp`);
     }
     return this.int.cmp(other.int);
   }
 
   eq(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in eq`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in eq`);
     }
     return this.int.eq(other.int);
   }
 
   gt(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in gt`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in gt`);
     }
     return this.int.gt(other.int);
   }
 
   gte(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in gte`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in gte`);
     }
     return this.int.gte(other.int);
   }
 
   lt(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in lt`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in lt`);
     }
     return this.int.lt(other.int);
   }
 
   lte(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in lte`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in lte`);
     }
     return this.int.lte(other.int);
   }
@@ -142,29 +154,33 @@ export class GN {
   //////////////////////////////////////////////////////////////*/
 
   add(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in add`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in add`);
     }
-    return new GN(this.int.plus(other.int).toFixed(0), this.decimals);
+    return new GN(this.int.plus(other.int).toFixed(0), this.resolution, this.base);
   }
 
   sub(other: GN) {
-    if (this.decimals !== other.decimals) {
-      throw new Error(`decimals must match, got (${this.decimals}, ${other.decimals}) in sub`);
+    if (this.scaler !== other.scaler) {
+      throw new Error(`scalers must match, got (${this.scaler}, ${other.scaler}) in sub`);
     }
-    return new GN(this.int.minus(other.int).toFixed(0), this.decimals);
+    return new GN(this.int.minus(other.int).toFixed(0), this.resolution, this.base);
   }
 
   mul(other: GN) {
-    return new GN(this.int.times(other.int).div(other.scaler).toFixed(0), this.decimals);
+    return new GN(this.int.times(other.int).div(other.scaler).toFixed(0), this.resolution, this.base);
   }
 
   div(other: GN) {
-    return new GN(this.int.times(other.scaler).div(other.int).toFixed(0), this.decimals);
+    return new GN(this.int.times(other.scaler).div(other.int).toFixed(0), this.resolution, this.base);
   }
 
   sqrt() {
-    return GN.fromDecimalBig(this.x().sqrt(), this.decimals);
+    return GN.fromDecimalBig(this.x().sqrt(), this.resolution);
+  }
+
+  square() {
+    return this.mul(this);
   }
 
   recklessMul(other: BigSource) {
@@ -172,7 +188,7 @@ export class GN {
     if (!isInteger(other)) {
       console.warn(`recklessMul by non-integer (${other.toString()}) wouldn't be possible in the EVM. Be careful!`);
     }
-    return new GN(this.int.times(other).toFixed(0), this.decimals);
+    return new GN(this.int.times(other).toFixed(0), this.resolution, this.base);
   }
 
   recklessDiv(other: BigSource) {
@@ -180,7 +196,7 @@ export class GN {
     if (!isInteger(other)) {
       console.warn(`recklessMul by non-integer (${other.toString()}) wouldn't be possible in the EVM. Be careful!`);
     }
-    return new GN(this.int.div(other).toFixed(0), this.decimals);
+    return new GN(this.int.div(other).toFixed(0), this.resolution, this.base);
   }
 
   /*//////////////////////////////////////////////////////////////
@@ -194,7 +210,7 @@ export class GN {
       case GNFormat.DECIMAL:
         return this.x().toFixed();
       case GNFormat.DECIMAL_WITH_TRAILING_ZEROS:
-        return this.x().toFixed(this.decimals);
+        return this.x().toFixed(this.approxDP());
       case GNFormat.LOSSY_HUMAN:
         // TODO: Bring logic in here instead of calling formatTokenAmount
         return formatTokenAmount(this.x().toNumber());
@@ -221,14 +237,19 @@ export class GN {
     return JSBI.BigInt(this.toString(GNFormat.INT));
   }
 
-  static zero(decimals: number) {
-    return new GN('0', decimals);
+  setResolution(resolution: number) {
+    return new GN(this.toString(GNFormat.INT), resolution, this.base);
+  }
+
+  static zero(decimals: number, base: 2 | 10 = 10) {
+    return new GN('0', decimals, base);
   }
 
   /**
    * Converts a fixed-point integer (stored as a BigNumber) to a `GN`
    * @param int The fixed-point integer as a BigNumber
-   * @param decimals The power of 10 used to compute the fixed-point scaling factor
+   * @param resolution The power of 2 or 10 used to compute the fixed-point scaling factor
+   * @param base Whether the scaling factor should be a power of 2 or 10
    * @returns Equivalent `GN`
    *
    * @example
@@ -238,14 +259,15 @@ export class GN {
    * const gn = GN.fromBigNumber(bn, decimals)
    * ```
    */
-  static fromBigNumber(int: BigNumber, decimals: number) {
-    return new GN(int.toString(), decimals);
+  static fromBigNumber(int: BigNumber, resolution: number, base: 2 | 10 = 10) {
+    return new GN(int.toString(), resolution, base);
   }
 
   /**
    * Converts a fixed-point integer (stored as a BigInt) to a `GN`
    * @param int The fixed-point integer as a JSBI.BigInt
-   * @param decimals The power of 10 used to compute the fixed-point scaling factor
+   * @param resolution The power of 2 or 10 used to compute the fixed-point scaling factor
+   * @param base Whether the scaling factor should be a power of 2 or 10
    * @returns Equivalent `GN`
    *
    * @example
@@ -254,8 +276,8 @@ export class GN {
    * amount.toString(GNFormat.LOSSLESS_DECIMAL); // 1.000001
    * ```
    */
-  static fromJSBI(int: JSBI, decimals: number) {
-    return new GN(int.toString(10), decimals);
+  static fromJSBI(int: JSBI, resolution: number, base: 2 | 10 = 10) {
+    return new GN(int.toString(10), resolution, base);
   }
 
   /**
@@ -266,7 +288,8 @@ export class GN {
    * @returns Equivalent `GN`
    */
   static fromDecimalBig(x: Big, decimals: number) {
-    return new GN(x.mul(scalerFor(decimals)).toFixed(0), decimals);
+    const base = 10;
+    return new GN(x.mul(scalerFor(base, decimals)).toFixed(0), decimals, base);
   }
 
   /**
