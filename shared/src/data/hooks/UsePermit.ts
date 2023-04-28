861a8c854bebe994e49e4f88f3fc695841c9cd33
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-04-28 15:46:28 -0500

    Add usePermit, useRedeem, and fix max redeem issues in withdraw modals (#401)
    * Add usePermit hook
    
    * Add useRedeem hook and revamp WithdrawModal
    
    * Refact WithdrawModal
    
    * Small fixes
    
    * Don't convertToShares when amount gt max and don't estimateGas for zeros
    
    * Fix pending txn modal stickiness
    
    * Address comments pt 1
    
    * Address comments pt 2

---
+++ shared/src/data/hooks/UsePermit.ts
@@ -0,0 +1,177 @@
+import { Address, Chain, useContractReads, useSignTypedData } from 'wagmi';
+import { useEffect, useMemo, useState } from 'react';
+import { erc20ABI } from '../../abis/ERC20';
+import { computeDomainSeparator } from '../../util/Permit';
+import { splitSignature } from 'ethers/lib/utils.js';
+
+export enum PermitState {
+  FETCHING_DATA,
+  READY_TO_SIGN,
+  ASKING_USER_TO_SIGN,
+  ERROR,
+  DONE,
+  DISABLED,
+}
+
+type uint256 = string;
+type address = string;
+
+type Permit = {
+  owner: address;
+  spender: address;
+  value: uint256;
+  nonce: uint256;
+  deadline: uint256;
+};
+
+const PERMIT_MESSAGE_TYPES = {
+  Permit: [
+    { name: 'owner', type: 'address' },
+    { name: 'spender', type: 'address' },
+    { name: 'value', type: 'uint256' },
+    { name: 'nonce', type: 'uint256' },
+    { name: 'deadline', type: 'uint256' },
+  ],
+};
+
+const SIGNATURE_SHELF_LIFE = 10 * 60; // seconds
+
+function evmCurrentTimePlus(secondsFromNow: number) {
+  return (Date.now() / 1000 + secondsFromNow).toFixed(0);
+}
+
+function attemptToInferDomain(
+  name: string | undefined,
+  chainId: number,
+  verifyingContract: Address,
+  domainSeparator: `0x${string}` | undefined
+) {
+  let domain: {
+    name?: string;
+    chainId: number;
+    verifyingContract: Address;
+    version: string;
+  } = { name, chainId, verifyingContract, version: '1' };
+
+  if (domainSeparator) {
+    for (const version of ['1', '2', '3']) {
+      domain = { chainId, verifyingContract, version, name };
+      if (computeDomainSeparator(domain) === domainSeparator) return { domain, success: true };
+
+      domain = { chainId, verifyingContract, version };
+      if (computeDomainSeparator(domain) === domainSeparator) return { domain, success: true };
+    }
+  }
+
+  return { domain, success: false };
+}
+
+export function usePermit(
+  chainId: number,
+  token: Address,
+  owner: Address,
+  spender: Address,
+  amount: string,
+  enabled = true
+) {
+  /*//////////////////////////////////////////////////////////////
+                            REACT STATE
+  //////////////////////////////////////////////////////////////*/
+  const [deadline, setDeadline] = useState<uint256>(evmCurrentTimePlus(SIGNATURE_SHELF_LIFE));
+
+  const erc20 = {
+    address: token,
+    abi: erc20ABI,
+    chainId: chainId,
+  };
+
+  const { data, isFetching, isError } = useContractReads({
+    contracts: [
+      { ...erc20, functionName: 'DOMAIN_SEPARATOR' },
+      { ...erc20, functionName: 'name' },
+      { ...erc20, functionName: 'nonces', args: [owner] },
+    ] as const,
+    allowFailure: false,
+    enabled: enabled,
+  });
+
+  /*//////////////////////////////////////////////////////////////
+                              REFRESHING
+  //////////////////////////////////////////////////////////////*/
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setDeadline(evmCurrentTimePlus(SIGNATURE_SHELF_LIFE)),
+      (SIGNATURE_SHELF_LIFE * 1000) / 4
+    );
+
+    return () => clearInterval(interval);
+  });
+
+  /*//////////////////////////////////////////////////////////////
+                              SIGNING
+  //////////////////////////////////////////////////////////////*/
+
+  const domainSeparator = data?.[0];
+  const name = data?.[1];
+  const nonce = data?.[2];
+  const { domain, success: compatible } = attemptToInferDomain(name, erc20.chainId, erc20.address, domainSeparator);
+
+  const permit: Permit | undefined = useMemo(() => {
+    if (nonce === undefined) return undefined;
+    return {
+      owner: owner,
+      spender: spender,
+      value: amount,
+      nonce: nonce.toString(),
+      deadline: deadline,
+    };
+  }, [owner, spender, amount, nonce, deadline]);
+
+  const {
+    signTypedData,
+    isLoading: isAskingUserToSign,
+    data: signature,
+    reset: resetSignature,
+  } = useSignTypedData({
+    domain,
+    types: PERMIT_MESSAGE_TYPES,
+    value: permit,
+  });
+
+  useEffect(() => {
+    resetSignature();
+  }, [resetSignature, permit]);
+
+  let state: PermitState;
+  let action: (() => void) | undefined;
+
+  if (!enabled) {
+    state = PermitState.DISABLED;
+    action = undefined;
+  } else if (isFetching) {
+    state = PermitState.FETCHING_DATA;
+    action = undefined;
+  } else if (isAskingUserToSign) {
+    state = PermitState.ASKING_USER_TO_SIGN;
+    action = undefined;
+  } else if (isError || !compatible) {
+    state = PermitState.ERROR;
+    action = undefined;
+  } else if (signature === undefined) {
+    state = PermitState.READY_TO_SIGN;
+    action = signTypedData;
+  } else {
+    state = PermitState.DONE;
+    action = undefined;
+  }
+
+  return {
+    state,
+    action,
+    result: {
+      deadline,
+      signature: signature ? splitSignature(signature) : undefined,
+    },
+  };
+}
