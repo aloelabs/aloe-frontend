3bf6b7c45884df33efedc9c1cdd50b0f0ba2fc42
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-05-08 17:49:56 -0500

    Eliminate API key dependence

--- earn/src/connector/WagmiProvider.tsx
+++ earn/src/connector/WagmiProvider.tsx
@@ -4,18 +4,48 @@ import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { infuraProvider } from 'wagmi/providers/infura';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [chain.mainnet, chain.optimism, chain.arbitrum, chain.goerli],
-  [
+let providers = [
+  jsonRpcProvider({
+    // ankr
+    rpc(targetChain) {
+      if (targetChain === chain.mainnet) return { http: 'https://rpc.ankr.com/eth' };
+      if (targetChain === chain.optimism) return { http: 'https://rpc.ankr.com/optimism' };
+      if (targetChain === chain.arbitrum) return { http: 'https://rpc.ankr.com/arbitrum' };
+      return null;
+    },
+    priority: 2,
+  }),
+  jsonRpcProvider({
+    // blast
+    rpc(targetChain) {
+      if (targetChain === chain.mainnet) return { http: 'https://eth-mainnet.public.blastapi.io' };
+      if (targetChain === chain.optimism) return { http: 'https://optimism-mainnet.public.blastapi.io' };
+      if (targetChain === chain.arbitrum) return { http: 'https://arbitrum-one.public.blastapi.io' };
+      return null;
+    },
+    priority: 3,
+  }),
+  publicProvider({ priority: 4 }),
+];
+
+if (process.env.REACT_APP_ALCHEMY_API_KEY) {
+  providers.push(
     alchemyProvider({
       apiKey: process.env.REACT_APP_ALCHEMY_API_KEY || '',
       priority: 0,
-    }),
-    infuraProvider({ apiKey: process.env.REACT_APP_INFURA_ID || '', priority: 1 }),
-    publicProvider({ priority: 2 }),
-  ],
+    })
+  );
+}
+if (process.env.REACT_APP_INFURA_ID) {
+  providers.push(infuraProvider({ apiKey: process.env.REACT_APP_INFURA_ID || '', priority: 1 }));
+}
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.optimism, chain.arbitrum, chain.goerli],
+  providers,
   { stallTimeout: 5000 }
 );
 
