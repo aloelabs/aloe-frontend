7a70055d00d57c07406c3eaad49b7019cdda0007
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-05-08 20:15:48 -0500

    Take advantage of user's metamask rpc if they've specified one

--- earn/src/connector/WagmiProvider.tsx
+++ earn/src/connector/WagmiProvider.tsx
@@ -1,43 +1,74 @@
+import { ethers } from 'ethers';
+import { DEFAULT_CHAIN } from 'shared/lib/data/constants/Values';
 import { WagmiConfig, chain, createClient, configureChains } from 'wagmi';
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { infuraProvider } from 'wagmi/providers/infura';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { publicProvider } from 'wagmi/providers/public';
 
-let providers = [
-  jsonRpcProvider({
-    // ankr
-    rpc(targetChain) {
-      if (targetChain === chain.mainnet) return { http: 'https://rpc.ankr.com/eth' };
-      if (targetChain === chain.optimism) return { http: 'https://rpc.ankr.com/optimism' };
-      if (targetChain === chain.arbitrum) return { http: 'https://rpc.ankr.com/arbitrum' };
-      return null;
-    },
-    priority: 2,
-  }),
-  jsonRpcProvider({
-    // blast
-    rpc(targetChain) {
-      if (targetChain === chain.mainnet) return { http: 'https://eth-mainnet.public.blastapi.io' };
-      if (targetChain === chain.optimism) return { http: 'https://optimism-mainnet.public.blastapi.io' };
-      if (targetChain === chain.arbitrum) return { http: 'https://arbitrum-one.public.blastapi.io' };
-      return null;
-    },
-    priority: 3,
-  }),
-  publicProvider({ priority: 4 }),
-];
+function fallbackProvider({ chainId }: { chainId?: number }) {
+  const targetChain = Object.values(chain).find((v) => v.id === chainId) || DEFAULT_CHAIN;
+  const config = {
+    ensAddress: targetChain.ens?.address,
+    chainId: targetChain.id,
+    name: targetChain.name,
+  };
 
-if (process.env.REACT_APP_ALCHEMY_API_KEY) {
-  providers.push(
-    alchemyProvider({
-      apiKey: process.env.REACT_APP_ALCHEMY_API_KEY || '',
+  const providers: ethers.providers.FallbackProviderConfig[] = [];
+  if (window.ethereum !== undefined) {
+    providers.push({
+      provider: new ethers.providers.Web3Provider(window.ethereum as ethers.providers.ExternalProvider, config),
       priority: 0,
-    })
-  );
+      stallTimeout: 5000,
+    });
+  }
+
+  switch (config.chainId) {
+    case chain.mainnet.id:
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth', config),
+        priority: 1,
+      });
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://eth-mainnet.public.blastapi.io', config),
+        priority: 1,
+      });
+      break;
+    case chain.optimism.id:
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/optimism', config),
+        priority: 1,
+      });
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://optimism-mainnet.public.blastapi.io', config),
+        priority: 1,
+      });
+      break;
+    case chain.arbitrum.id:
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/arbitrum', config),
+        priority: 1,
+      });
+      providers.push({
+        provider: new ethers.providers.JsonRpcProvider('https://arbitrum-one.public.blastapi.io', config),
+        priority: 1,
+      });
+      break;
+    default:
+      throw new Error('Unsupported network');
+  }
+
+  return new ethers.providers.FallbackProvider(providers, 1);
+}
+
+const hasNonPublicRpc = ['REACT_APP_ALCHEMY_API_KEY', 'REACT_APP_INFURA_ID'].some(
+  (key) => process.env[key] !== undefined
+);
+const providers = [publicProvider({ priority: 2 })];
+if (process.env.REACT_APP_ALCHEMY_API_KEY) {
+  providers.push(alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_API_KEY || '', priority: 0 }));
 }
 if (process.env.REACT_APP_INFURA_ID) {
   providers.push(infuraProvider({ apiKey: process.env.REACT_APP_INFURA_ID || '', priority: 1 }));
@@ -65,8 +96,9 @@ const client = createClient({
       options: { appName: 'Aloe II' },
     }),
   ],
-  provider,
-  webSocketProvider,
+  // @ts-ignore
+  provider: hasNonPublicRpc ? provider : fallbackProvider,
+  webSocketProvider: hasNonPublicRpc ? webSocketProvider : undefined,
 });
 
 export type WagmiProviderProps = {
