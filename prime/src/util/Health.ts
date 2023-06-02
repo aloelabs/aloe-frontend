f2ad48f864d482e0382f6087d88fe52541a8f4e1
ianwoodard <ianwoodard2000@gmail.com>
2023-06-01 20:31:49 -0700

    Reworking how we display health

--- prime/src/util/Health.ts
+++ prime/src/util/Health.ts
@@ -1,9 +1,15 @@
+export const HEALTH_LOW_COLOR = 'rgba(235, 87, 87, 1)';
+export const HEALTH_MEDIUM_COLOR = 'rgba(242, 201, 76, 1)';
+export const HEALTH_HIGH_COLOR = 'rgba(0, 193, 67, 1)';
+export const HEALTH_LOW_THRESHOLD = 1.05;
+export const HEALTH_MEDIUM_THRESHOLD = 1.5;
+
 export function getHealthColor(health: number): string {
-  if (health <= 1.1) {
-    return '#EB5757';
-  } else if (health <= 1.25) {
-    return '#F2C94C';
+  if (health < HEALTH_LOW_THRESHOLD) {
+    return HEALTH_LOW_COLOR;
+  } else if (health < HEALTH_MEDIUM_THRESHOLD) {
+    return HEALTH_MEDIUM_COLOR;
   } else {
-    return '#00C143';
+    return HEALTH_HIGH_COLOR;
   }
 }
