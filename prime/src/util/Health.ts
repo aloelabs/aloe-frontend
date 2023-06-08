369431eb5335578244afa166de49eaf667b2a788
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-06-07 21:48:23 -0700

    Updating medium health threshold

--- prime/src/util/Health.ts
+++ prime/src/util/Health.ts
@@ -2,7 +2,7 @@ export const HEALTH_LOW_COLOR = 'rgba(235, 87, 87, 1)';
 export const HEALTH_MEDIUM_COLOR = 'rgba(242, 201, 76, 1)';
 export const HEALTH_HIGH_COLOR = 'rgba(0, 193, 67, 1)';
 export const HEALTH_LOW_THRESHOLD = 1.05;
-export const HEALTH_MEDIUM_THRESHOLD = 1.5;
+export const HEALTH_MEDIUM_THRESHOLD = 1.4;
 
 export function getHealthColor(health: number): string {
   if (health < HEALTH_LOW_THRESHOLD) {
