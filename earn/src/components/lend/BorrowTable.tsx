5b1b91d1ec6e4cc1916f58beeea923712e078d44
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-07-16 11:20:02 -0700

    Adding borrow table

--- earn/src/components/lend/BorrowTable.tsx
+++ earn/src/components/lend/BorrowTable.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 
 import TokenIcon from '../common/TokenIcon';
 
+const HEALTH_GREEN = 'rgba(0, 193, 67, 1)';
+const HEALTH_YELLOW = 'rgba(242, 201, 76, 1)';
+const HEALTH_RED = 'rgba(235, 87, 87, 1)';
+const HEALTH_DEFAULT = 'rgba(130, 160, 182, 1)';
+
 const TableContainer = styled.div`
   width: 100%;
   overflow-x: auto;
@@ -21,18 +26,42 @@ const TableHeader = styled.thead`
   text-align: start;
 `;
 
-export type CollateralTableRow = {
+const HealthDot = styled.div.attrs((props: { color: string }) => props)`
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+export type BorrowTableRow = {
   asset: Token;
   balance: number;
   balanceUsd: number;
+  apr: number;
+  health: number;
+  source: string;
 };
 
-export type CollateralTableProps = {
-  rows: CollateralTableRow[];
+export type BorrowTableProps = {
+  rows: BorrowTableRow[];
 };
 
-export default function CollateralTable(props: CollateralTableProps) {
+// TODO: Make this a shared component
+function getHealthColor(health: number) {
+  if (health === -1) {
+    return HEALTH_DEFAULT;
+  } else if (health <= 1.02) {
+    return HEALTH_RED;
+  } else if (health <= 1.25) {
+    return HEALTH_YELLOW;
+  } else {
+    return HEALTH_GREEN;
+  }
+}
+
+export default function BorrowTable(props: BorrowTableProps) {
   const { rows } = props;
+
   return (
     <TableContainer>
       <Table>
@@ -46,6 +75,21 @@ export default function CollateralTable(props: CollateralTableProps) {
             <th className='px-4 py-2 text-end'>
               <Text size='S' weight='bold'>
                 Balance
+              </Text>
+            </th>
+            <th className='px-4 py-2 text-end'>
+              <Text size='S' weight='bold'>
+                APR
+              </Text>
+            </th>
+            <th className='px-4 py-2 text-center'>
+              <Text size='S' weight='bold'>
+                Health
+              </Text>
+            </th>
+            <th className='px-4 py-2 text-end'>
+              <Text size='S' weight='bold'>
+                Source
               </Text>
             </th>
           </tr>
@@ -64,6 +108,17 @@ export default function CollateralTable(props: CollateralTableProps) {
                 <Display size='XXS' color='rgba(130, 160, 182, 1)'>
                   {formatTokenAmount(row.balance)} {row.asset.symbol}
                 </Display>
+              </td>
+              <td className='px-4 py-2 text-end'>
+                <Display size='XS'>{row.apr.toFixed(2)}%</Display>
+              </td>
+              <td className='px-4 py-2'>
+                <div className='flex justify-center items-center'>
+                  <HealthDot color={getHealthColor(row.health)} />
+                </div>
+              </td>
+              <td className='px-4 py-2 text-end'>
+                <Display size='XS'>{row.source}</Display>
               </td>
             </tr>
           ))}
--- earn/src/pages/MarketsPage.tsx
+++ earn/src/pages/MarketsPage.tsx
@@ -1,15 +1,28 @@
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 
 import axios, { AxiosResponse } from 'axios';
+import { ethers } from 'ethers';
 import AppPage from 'shared/lib/components/common/AppPage';
 import { Text } from 'shared/lib/components/common/Typography';
 import { Token } from 'shared/lib/data/Token';
-import { getTokenBySymbol } from 'shared/lib/data/TokenData';
-import { useAccount, useProvider } from 'wagmi';
+import { getToken, getTokenBySymbol } from 'shared/lib/data/TokenData';
+import { roundPercentage } from 'shared/lib/util/Numbers';
+import { Address, useAccount, useContract, useProvider } from 'wagmi';
 
 import { ChainContext } from '../App';
+import KittyLensAbi from '../assets/abis/KittyLens.json';
+import MarginAccountLensABI from '../assets/abis/MarginAccountLens.json';
+import UniswapV3PoolABI from '../assets/abis/UniswapV3Pool.json';
+import BorrowTable, { BorrowTableRow } from '../components/lend/BorrowTable';
 import CollateralTable, { CollateralTableRow } from '../components/lend/CollateralTable';
 import SupplyTable, { SupplyTableRow } from '../components/lend/SupplyTable';
+import {
+  ALOE_II_BORROWER_LENS_ADDRESS,
+  ALOE_II_FACTORY_ADDRESS,
+  ALOE_II_LENDER_LENS_ADDRESS,
+  UNISWAP_POOL_DENYLIST,
+} from '../data/constants/Addresses';
+import { TOPIC0_CREATE_MARKET_EVENT } from '../data/constants/Signatures';
 import { API_PRICE_RELAY_LATEST_URL } from '../data/constants/Values';
 import {
   getAvailableLendingPairs,
@@ -17,6 +30,8 @@ import {
   LendingPair,
   LendingPairBalances,
 } from '../data/LendingPair';
+import { MarginAccount, UniswapPoolInfo, fetchMarginAccounts } from '../data/MarginAccount';
+import { MarketInfo, fetchMarketInfoFor } from '../data/MarketInfo';
 import { PriceRelayLatestResponse } from '../data/PriceRelayResponse';
 
 export type TokenQuote = {
@@ -39,11 +54,20 @@ export default function MarketsPage() {
   const [tokenQuotes, setTokenQuotes] = useState<TokenQuote[]>([]);
   const [lendingPairs, setLendingPairs] = useState<LendingPair[]>([]);
   const [lendingPairBalances, setLendingPairBalances] = useState<LendingPairBalances[]>([]);
+  const [availablePools, setAvailablePools] = useState<Map<string, UniswapPoolInfo>>(new Map());
+  const [marginAccounts, setMarginAccounts] = useState<MarginAccount[]>([]);
+  const [marketInfos, setMarketInfos] = useState<MarketInfo[]>([]);
 
   // MARK: wagmi hooks
   const account = useAccount();
   const provider = useProvider({ chainId: activeChain?.id });
-  const address = account.address;
+  const userAddress = account.address;
+
+  const borrowerLensContract = useContract({
+    abi: MarginAccountLensABI,
+    address: ALOE_II_BORROWER_LENS_ADDRESS,
+    signerOrProvider: provider,
+  });
 
   const uniqueSymbols = useMemo(() => {
     const symbols = new Set<string>();
@@ -86,6 +110,7 @@ export default function MarketsPage() {
     };
   }, [activeChain, tokenQuotes, tokenQuotes.length, uniqueSymbols]);
 
+  // MARK: Fetch lending pairs
   useEffect(() => {
     let mounted = true;
     async function fetch() {
@@ -98,13 +123,14 @@ export default function MarketsPage() {
     return () => {
       mounted = false;
     };
-  }, [provider, address, activeChain]);
+  }, [provider, userAddress, activeChain]);
 
+  // MARK: Fetch lending pair balances
   useEffect(() => {
     let mounted = true;
     async function fetch() {
-      if (!address) return;
-      const results = await Promise.all(lendingPairs.map((p) => getLendingPairBalances(p, address, provider)));
+      if (!userAddress) return;
+      const results = await Promise.all(lendingPairs.map((p) => getLendingPairBalances(p, userAddress, provider)));
       if (mounted) {
         setLendingPairBalances(results);
       }
@@ -113,7 +139,94 @@ export default function MarketsPage() {
     return () => {
       mounted = false;
     };
-  }, [provider, address, lendingPairs]);
+  }, [provider, userAddress, lendingPairs]);
+
+  // MARK: Fetch available pools
+  useEffect(() => {
+    let mounted = true;
+    async function fetchAvailablePools() {
+      let logs: ethers.providers.Log[] = [];
+      try {
+        logs = await provider.getLogs({
+          fromBlock: 0,
+          toBlock: 'latest',
+          address: ALOE_II_FACTORY_ADDRESS,
+          topics: [TOPIC0_CREATE_MARKET_EVENT],
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      const poolAddresses = logs
+        .map((e) => `0x${e.topics[1].slice(-40)}`)
+        .filter((addr) => {
+          return !UNISWAP_POOL_DENYLIST.includes(addr.toLowerCase());
+        });
+      const poolInfoTuples = await Promise.all(
+        poolAddresses.map((addr) => {
+          const poolContract = new ethers.Contract(addr, UniswapV3PoolABI, provider);
+          return Promise.all([poolContract.token0(), poolContract.token1(), poolContract.fee()]);
+        })
+      );
+
+      const poolInfoMap = new Map<string, UniswapPoolInfo>();
+      poolAddresses.forEach((addr, i) => {
+        const token0 = getToken(activeChain.id, poolInfoTuples[i][0] as Address);
+        const token1 = getToken(activeChain.id, poolInfoTuples[i][1] as Address);
+        const fee = poolInfoTuples[i][2] as number;
+        if (token0 && token1) poolInfoMap.set(addr.toLowerCase(), { token0, token1, fee });
+      });
+
+      if (mounted) setAvailablePools(poolInfoMap);
+    }
+
+    fetchAvailablePools();
+    return () => {
+      mounted = false;
+    };
+  }, [activeChain, provider]);
+
+  // MARK: Fetch margin accounts
+  useEffect(() => {
+    let mounted = true;
+    async function fetch() {
+      if (borrowerLensContract == null || userAddress === undefined || availablePools.size === 0) return;
+      const marginAccounts = await fetchMarginAccounts(activeChain, provider, userAddress, availablePools);
+      if (mounted) {
+        setMarginAccounts(marginAccounts);
+      }
+    }
+    fetch();
+    return () => {
+      mounted = false;
+    };
+  }, [userAddress, activeChain, borrowerLensContract, provider, availablePools]);
+
+  // MARK: Fetch market info
+  useEffect(() => {
+    let mounted = true;
+    async function fetch() {
+      const lenderLensContract = new ethers.Contract(ALOE_II_LENDER_LENS_ADDRESS, KittyLensAbi, provider);
+      const results = await Promise.all(
+        marginAccounts.map((account) => {
+          return fetchMarketInfoFor(
+            lenderLensContract,
+            account.lender0,
+            account.lender1,
+            account.token0.decimals,
+            account.token1.decimals
+          );
+        })
+      );
+      if (mounted) {
+        setMarketInfos(results);
+      }
+    }
+    fetch();
+    return () => {
+      mounted = false;
+    };
+  }, [marginAccounts, provider]);
 
   const combinedBalances: TokenBalance[] = useMemo(() => {
     if (tokenQuotes.length === 0) {
@@ -218,14 +331,51 @@ export default function MarketsPage() {
     return rows;
   }, [tokenBalances]);
 
+  const borrowRows = useMemo(() => {
+    const rows: BorrowTableRow[] = [];
+    marginAccounts.forEach((marginAccount) => {
+      const token0Price = tokenQuotes.find((quote) => quote.token.address === marginAccount.token0.address)?.price || 0;
+      const token1Price = tokenQuotes.find((quote) => quote.token.address === marginAccount.token1.address)?.price || 0;
+      const marketInfo = marketInfos.find(
+        (info) => info.lender0 === marginAccount.lender0 && info.lender1 === marginAccount.lender1
+      );
+      const borrowerAPR0 = marketInfo?.borrowerAPR0 || 0;
+      const borrowerAPR1 = marketInfo?.borrowerAPR1 || 0;
+      const health = Object.values(marginAccount.assets).every((value) => value === 0) ? -1 : marginAccount.health;
+      rows.push({
+        asset: marginAccount.token0,
+        balance: marginAccount.liabilities.amount0,
+        balanceUsd: marginAccount.liabilities.amount0 * token0Price,
+        apr: roundPercentage(borrowerAPR0 * 100, 2),
+        health: health,
+        source: marginAccount.token0.symbol + '/' + marginAccount.token1.symbol,
+      });
+      rows.push({
+        asset: marginAccount.token1,
+        balance: marginAccount.liabilities.amount1,
+        balanceUsd: marginAccount.liabilities.amount1 * token1Price,
+        apr: roundPercentage(borrowerAPR1 * 100, 2),
+        health: health,
+        source: marginAccount.token0.symbol + '/' + marginAccount.token1.symbol,
+      });
+    });
+    return rows;
+  }, [marginAccounts, marketInfos, tokenQuotes]);
+
   return (
     <AppPage>
       <div className='flex flex-col gap-6 max-w-screen-2xl m-auto'>
         <Text size='XL'>Supply</Text>
         <SupplyTable rows={supplyRows} />
-        <div className='flex flex-col gap-6'>
-          <Text size='XL'>Collateral</Text>
-          <CollateralTable rows={collateralRows} />
+        <div className='flex gap-6'>
+          <div className='flex flex-col gap-6 w-1/3'>
+            <Text size='XL'>Collateral</Text>
+            <CollateralTable rows={collateralRows} />
+          </div>
+          <div className='flex flex-col gap-6 w-2/3'>
+            <Text size='XL'>Borrow</Text>
+            <BorrowTable rows={borrowRows} />
+          </div>
         </div>
       </div>
     </AppPage>
