a2e0daa99c7bf083db737c01cc5d3a05b705dafc
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-08-03 20:42:01 -0700

    Fixing boost tooltip

--- earn/src/components/boost/LiquidityChartTooltip.tsx
+++ earn/src/components/boost/LiquidityChartTooltip.tsx
@@ -10,12 +10,14 @@ export const PERCENTAGE_WIDTH = 66;
 const TOOLTIP_BG_COLOR = 'rgba(13, 23, 30, 1)';
 const TOOLTIP_BORDER_COLOR = GREY_700;
 
-const TooltipContainer = styled.div.attrs((props: { offset: number }) => props)`
+const TooltipContainer = styled.div.attrs((props: { offset: number; chartWidth: number }) => props)`
   ${tw`rounded-md shadow-md`}
   position: absolute;
   top: 0;
   left: 50%;
-  transform: translateX(clamp(0px, ${(props) => props.offset - PERCENTAGE_WIDTH / 2}px, 225px));
+  transform: translateX(
+    clamp(0px, ${(props) => props.offset - PERCENTAGE_WIDTH / 2}px, ${(props) => props.chartWidth - PERCENTAGE_WIDTH}px)
+  );
   border: 1px solid ${TOOLTIP_BORDER_COLOR};
   width: ${PERCENTAGE_WIDTH}px;
   box-shadow: 0px 8px 32px 0px rgba(0, 0, 0, 0.12);
@@ -29,12 +31,13 @@ export default function LiquidityChartTooltip(props: {
   selectedPrice: number;
   currentPrice: number;
   x: number;
+  chartWidth: number;
 }) {
-  const { active, selectedPrice, currentPrice, x } = props;
+  const { active, selectedPrice, currentPrice, x, chartWidth } = props;
   if (active) {
     const percentChange = ((selectedPrice - currentPrice) / currentPrice) * 100 || 0;
     return (
-      <TooltipContainer offset={x}>
+      <TooltipContainer offset={x} chartWidth={chartWidth}>
         <div className='flex flex-col justify-center items-center'>
           <Text size='S' weight='bold'>
             {(percentChange > 0 ? '+' : '') + roundPercentage(percentChange, 1)}%
