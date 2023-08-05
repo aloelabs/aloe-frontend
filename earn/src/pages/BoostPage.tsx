2c5ec9ba7711e59cfc50e67b84d994dbaab7a694
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-08-04 21:59:18 -0700

    Adding boost card component (#553)

--- earn/src/pages/BoostPage.tsx
+++ earn/src/pages/BoostPage.tsx
@@ -3,20 +3,12 @@ import { useContext, useEffect, useMemo, useState } from 'react';
 import JSBI from 'jsbi';
 import { UniswapV3PoolABI } from 'shared/lib/abis/UniswapV3Pool';
 import AppPage from 'shared/lib/components/common/AppPage';
-import { FilledGradientButton, FilledGreyButton } from 'shared/lib/components/common/Buttons';
-import { Display, Text } from 'shared/lib/components/common/Typography';
-import { formatTokenAmount, roundPercentage, toBig } from 'shared/lib/util/Numbers';
+import { Text } from 'shared/lib/components/common/Typography';
+import { toBig } from 'shared/lib/util/Numbers';
 import { useAccount, useContractReads, useProvider } from 'wagmi';
 
 import { ChainContext } from '../App';
-import LiquidityChart from '../components/boost/LiquidityChart';
-import TokenPairIcons from '../components/common/TokenPairIcons';
-import {
-  InRangeBadge,
-  OutOfRangeBadge,
-  UniswapPositionCardContainer,
-  UniswapPositionCardWrapper,
-} from '../components/common/UniswapPositionCard';
+import BoostCard from '../components/boost/BoostCard';
 import { sqrtRatioToPrice } from '../data/BalanceSheet';
 import {
   UniswapNFTPosition,
@@ -27,8 +19,6 @@ import {
   tickToPrice,
 } from '../data/Uniswap';
 import { getProminentColor } from '../util/Colors';
-
-const ACCENT_COLOR = 'rgba(130, 160, 182, 1)';
 
 export default function BoostPage() {
   const { activeChain } = useContext(ChainContext);
@@ -148,91 +138,25 @@ export default function BoostPage() {
       <Text size='XL'>Boost</Text>
       <div className='flex flex-wrap gap-4 mt-4'>
         {uniswapNFTCardInfo.map((position, index) => {
-          const {
-            token0,
-            token1,
-            minPrice,
-            maxPrice,
-            amount0,
-            amount1,
-            amount0Percent,
-            amount1Percent,
-            isInRange,
-            isDeposit,
-          } = position;
-
           return (
-            <UniswapPositionCardContainer key={index}>
-              <UniswapPositionCardWrapper>
-                <div className='flex flex-col gap-4'>
-                  <div className='flex justify-center items-center'>
-                    <TokenPairIcons
-                      token0IconPath={token0.logoURI}
-                      token1IconPath={token1.logoURI}
-                      token0AltText={`${token0.symbol}'s icon`}
-                      token1AltText={`${token1.symbol}'s icon`}
-                    />
-                  </div>
-                  <div className='flex justify-between'>
-                    <div className='text-left'>
-                      <Display size='XS' color={ACCENT_COLOR}>
-                        {roundPercentage(amount0Percent, 1)}%
-                      </Display>
-                      <Display size='S'>{formatTokenAmount(amount0, 5)}</Display>
-                      <Text size='XS'>{token0.symbol}</Text>
-                    </div>
-                    <div className='text-right'>
-                      <Display size='XS' color={ACCENT_COLOR}>
-                        {roundPercentage(amount1Percent, 1)}%
-                      </Display>
-                      <Display size='S'>{formatTokenAmount(amount1, 5)}</Display>
-                      <Text size='XS'>{token1.symbol}</Text>
-                    </div>
-                  </div>
-                  <div className='flex justify-between'>
-                    <div className='text-left'>
-                      <Text size='S' color={ACCENT_COLOR}>
-                        Min Price
-                      </Text>
-                      <Display size='S'>{formatTokenAmount(minPrice, 5)}</Display>
-                      <Text size='XS'>
-                        {token1.symbol} per {token0.symbol}
-                      </Text>
-                    </div>
-                    <div className='text-right'>
-                      <Text size='S' color={ACCENT_COLOR}>
-                        Max Price
-                      </Text>
-                      <Display size='S'>{formatTokenAmount(maxPrice, 5)}</Display>
-                      <Text size='XS'>
-                        {token1.symbol} per {token0.symbol}
-                      </Text>
-                    </div>
-                  </div>
-                  <div className='flex justify-between'>
-                    {isInRange ? <InRangeBadge /> : <OutOfRangeBadge />}
-                    {isDeposit ? (
-                      <FilledGradientButton size='S' onClick={() => {}}>
-                        Lever Up
-                      </FilledGradientButton>
-                    ) : (
-                      <FilledGreyButton size='S' onClick={() => {}}>
-                        Manage
-                      </FilledGreyButton>
-                    )}
-                  </div>
-                </div>
-                <LiquidityChart
-                  poolAddress={position.poolAddress}
-                  minPrice={position.minPrice}
-                  maxPrice={position.maxPrice}
-                  currentPrice={position.currentPrice}
-                  color0={colors.get(index)?.[0] ?? 'red'}
-                  color1={colors.get(index)?.[1] ?? 'blue'}
-                  uniqueId={index.toString()}
-                />
-              </UniswapPositionCardWrapper>
-            </UniswapPositionCardContainer>
+            <BoostCard
+              key={index}
+              token0={position.token0}
+              token1={position.token1}
+              minPrice={position.minPrice}
+              maxPrice={position.maxPrice}
+              currentPrice={position.currentPrice}
+              amount0={position.amount0}
+              amount1={position.amount1}
+              amount0Percent={position.amount0Percent}
+              amount1Percent={position.amount1Percent}
+              isInRange={position.isInRange}
+              isDeposit={position.isDeposit}
+              poolAddress={position.poolAddress}
+              color0={colors.get(index)?.[0] ?? 'red'}
+              color1={colors.get(index)?.[1] ?? 'blue'}
+              uniqueId={index.toString()}
+            />
           );
         })}
       </div>
