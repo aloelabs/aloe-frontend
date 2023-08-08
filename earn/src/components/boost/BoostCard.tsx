29789ad37359ba84d88276b826ff2f66419c637c
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-08-07 21:32:20 -0500

    Add boost multiplier label and propose alt button style

--- earn/src/components/boost/BoostCard.tsx
+++ earn/src/components/boost/BoostCard.tsx
@@ -14,11 +14,124 @@ import {
 } from '../common/UniswapPositionCard';
 import LiquidityChart from './LiquidityChart';
 
+import { ReactComponent as WrenchIcon } from '../../assets/svg/wrench.svg';
+import { ReactComponent as ZapIcon } from '../../assets/svg/zap.svg';
+
 export const ACCENT_COLOR = 'rgba(130, 160, 182, 1)';
+const BOOSTED_BACKGROUND_COLOR = 'rgb(255,217,102,0.1)';
+const BOOSTED_TEXT_COLOR = 'rgb(255,217,102)';
+const PLAIN_BACKGROUND_COLOR = 'rgba(150,150,150,0.1)';
+const PLAIN_TEXT_COLOR = 'rgb(150,150,150)';
 
 const CustomUniswapPositionCardContainer = styled(UniswapPositionCardContainer)`
   width: 300px;
+  position: relative;
+  border-radius: 8px;
+
+  &:after {
+    content: '';
+    position: absolute;
+    background-color: rgba(15, 23, 29, 0.5);
+
+    width: 36px;
+    height: 36px;
+    top: 14px;
+    right: 14px;
+  }
+
+  &:hover {
+    // box-shadow: 0px 0px 16px 0px rgba(255, 255, 255, 0.16);
+    // outline: 0.5px solid white;
+
+    &:after {
+      content: none;
+    }
+  }
 `;
+
+const CardActionButton = styled.button.attrs((props: { shouldAnimate: boolean }) => props)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 1px solid white;
+  top: 16px;
+  right: 16px;
+
+  mask-origin: content-box;
+  mask-image: linear-gradient(
+    20deg,
+    rgba(255, 255, 255, 0.8) 0% 33%,
+    rgba(255, 255, 255, 1) 45% 55%,
+    rgba(255, 255, 255, 0.8) 66% 100%
+  );
+  mask-size: cover;
+
+  ${(props) => (props.shouldAnimate ? 'animation: pulse 1.2s linear infinite' : '')};
+  @keyframes pulse {
+    0% {
+      transform: rotate(0deg);
+      mask-position: 0px -16px;
+    }
+    35% {
+      transform: rotate(0deg);
+      mask-position: 0px -16px;
+    }
+    45% {
+      transform: rotate(10deg);
+      mask-position: 0px 4px;
+    }
+    55% {
+      transform: rotate(-5deg);
+      mask-position: 0px -4px;
+    }
+    65% {
+      transform: rotate(0deg);
+      mask-position: 0px 16px;
+    }
+    100% {
+      transform: rotate(0deg);
+      mask-position: 0px 16px;
+    }
+  }
+
+  svg {
+    padding: 8px;
+    path {
+      stroke: white;
+      stroke-width: 1px;
+      fill: white;
+    }
+  }
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`;
+
+const BoostBadgeWrapper = styled.div.attrs((props: { isBoosted: boolean }) => props)`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  background-color: ${(props) => (props.isBoosted ? BOOSTED_BACKGROUND_COLOR : PLAIN_BACKGROUND_COLOR)};
+  align-items: center;
+  width: fit-content;
+  height: 28px;
+  padding: 4px 8px;
+  border-radius: 8px;
+`;
+
+function BoostBadge(props: { boostFactor: number | null }) {
+  return (
+    <BoostBadgeWrapper isBoosted={props.boostFactor !== null}>
+      <Text size='S' color={props.boostFactor !== null ? BOOSTED_TEXT_COLOR : PLAIN_TEXT_COLOR}>
+        {props.boostFactor !== null ? `${props.boostFactor.toFixed(1)}x Boost ⚡️` : 'No Boost'}
+      </Text>
+    </BoostBadgeWrapper>
+  );
+}
 
 export type UniswapPositionCardProps = {
   info: BoostCardInfo;
@@ -33,20 +146,22 @@ export default function BoostCard(props: UniswapPositionCardProps) {
 
   const editButton =
     info.cardType === BoostCardType.UNISWAP_NFT ? (
-      <FilledGradientButton size='S' onClick={() => {}}>
-        Lever Up
-      </FilledGradientButton>
+      <CardActionButton onClick={setSelectedPosition} disabled={false} shouldAnimate={true}>
+        <ZapIcon width={32} height={32} />
+      </CardActionButton>
     ) : (
-      <FilledGreyButton size='S' onClick={setSelectedPosition}>
-        Manage
-      </FilledGreyButton>
+      <CardActionButton onClick={setSelectedPosition} disabled={false} shouldAnimate={false}>
+        <WrenchIcon width={32} height={32} />
+      </CardActionButton>
     );
 
   const minPrice = tickToPrice(info.position.lower, token0.decimals, token1.decimals, true);
   const maxPrice = tickToPrice(info.position.upper, token0.decimals, token1.decimals, true);
+  const boostFactor = info.boostFactor();
 
   return (
     <CustomUniswapPositionCardContainer>
+      {!isDisplayOnly && editButton}
       <UniswapPositionCardWrapper>
         <div className='flex flex-col gap-4'>
           <div className='flex justify-center items-center'>
@@ -93,9 +208,9 @@ export default function BoostCard(props: UniswapPositionCardProps) {
               </Text>
             </div>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex flex-col items-center gap-2 mb-2'>
             {info.isInRange() ? <InRangeBadge /> : <OutOfRangeBadge />}
-            {!isDisplayOnly && editButton}
+            <BoostBadge boostFactor={boostFactor} />
           </div>
         </div>
         <LiquidityChart
