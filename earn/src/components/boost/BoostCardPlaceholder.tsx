e1ca87645525b6fc7287bec510debdda3937a254
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-08-16 20:51:47 -0700

    Fixing boost card placeholder height

--- earn/src/components/boost/BoostCardPlaceholder.tsx
+++ earn/src/components/boost/BoostCardPlaceholder.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const BoostCardPlaceholder = styled.div`
   width: 300px;
-  height: 423.9px;
+  height: 461px;
   background: #0d171e;
   background-image: linear-gradient(to right, #0d171e 0%, #131f28 20%, #0d171e 40%, #0d171e 100%);
   background-repeat: no-repeat;
-  background-size: 100% 423.9px;
+  background-size: 100% 461px;
   border-radius: 8px;
   display: inline-block;
   animation: boostCardShimmer 0.75s forwards linear infinite;
