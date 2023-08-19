3204d76bf49ee4801a98b63b7dd0be6c4ca68e98
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-08-19 16:59:00 -0700

    Adding sentry environments (#591)

--- prime/src/index.tsx
+++ prime/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { DEVELOPMENT, PRODUCTION } from 'shared/lib/data/constants/SentryEnvironments';
+import { isProduction } from 'shared/lib/util/Utils';
 
 import App from './App';
 import generatedGitInfo from './gitInfo.json';
@@ -11,6 +13,7 @@ import generatedGitInfo from './gitInfo.json';
 if (process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: isProduction() ? PRODUCTION : DEVELOPMENT,
     integrations: [new Sentry.BrowserTracing()],
     release: generatedGitInfo.commit || undefined,
     sampleRate: 1.0,
