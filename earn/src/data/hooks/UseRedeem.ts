ad4456a1d6beb46438bb0e5a7f54e2b6e7f138b9
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-09-23 17:07:23 -0500

    Take advantage of new contract feature to simplify useRedeem hook

--- earn/src/data/hooks/UseRedeem.ts
+++ earn/src/data/hooks/UseRedeem.ts
@@ -1,8 +1,7 @@
-import { BigNumber, Signature } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import { routerABI } from 'shared/lib/abis/Router';
 import { ALOE_II_ROUTER_ADDRESS } from 'shared/lib/data/constants/ChainSpecific';
 import { GN } from 'shared/lib/data/GoodNumber';
-import { PermitState, usePermit } from 'shared/lib/data/hooks/UsePermit';
 import {
   Address,
   erc4626ABI,
@@ -20,15 +19,6 @@ export enum RedeemState {
   READY_TO_REDEEM,
   ASKING_USER_TO_REDEEM,
 }
-
-const PERMIT_STATE_TO_REDEEM_STATE = {
-  [PermitState.FETCHING_DATA]: RedeemState.FETCHING_DATA,
-  [PermitState.READY_TO_SIGN]: RedeemState.READY_TO_SIGN,
-  [PermitState.ASKING_USER_TO_SIGN]: RedeemState.ASKING_USER_TO_SIGN,
-  [PermitState.ERROR]: undefined,
-  [PermitState.DONE]: undefined,
-  [PermitState.DISABLED]: undefined,
-};
 
 const BN0 = BigNumber.from('0');
 const GAS_LIMIT_CUSHION = 110;
@@ -62,7 +52,7 @@ export function useRedeem(chainId: number, lender: Address, amount: GN, owner: A
   });
   const [maxAmount, maxShares, maxSharesIsChanging] = maxData ?? [BN0, BN0, false];
 
-  // If the user is trying to redeem more than they can, we'll just redeem the max.
+  // If the user is trying to redeem more than they have, we'll just redeem the max.
   // This means we won't refetch multiple times if the user enters a number greater than the max.
   const amountToConvert = amount.toBigNumber().lte(maxAmount) ? amount : GN.Q(112);
   const { data: sharesData, isFetching: isFetchingShares } = useContractRead({
@@ -72,8 +62,8 @@ export function useRedeem(chainId: number, lender: Address, amount: GN, owner: A
   });
   const shares = sharesData ?? BN0;
 
-  const threshold = maxShares.mul(95).div(100);
-  const shouldUseChecks = shares.gte(threshold) && maxSharesIsChanging;
+  const threshold = maxShares.mul(999).div(1000);
+  const shouldRedeemMax = shares.gte(threshold) && maxSharesIsChanging;
 
   /*//////////////////////////////////////////////////////////////
                             ERC4626 REDEEM
@@ -83,8 +73,8 @@ export function useRedeem(chainId: number, lender: Address, amount: GN, owner: A
   const { config: configRedeem } = usePrepareContractWrite({
     ...erc4626,
     functionName: 'redeem',
-    args: [redeemableShares, owner, recipient],
-    enabled: !shouldUseChecks && redeemableShares.gt(0),
+    args: [shouldRedeemMax ? ethers.constants.MaxUint256 : redeemableShares, owner, recipient],
+    enabled: redeemableShares.gt(0),
   });
   const {
     write: redeem,
@@ -101,41 +91,6 @@ export function useRedeem(chainId: number, lender: Address, amount: GN, owner: A
       : undefined,
   });
 
-  /*//////////////////////////////////////////////////////////////
-                            ROUTER REDEEM
-  //////////////////////////////////////////////////////////////*/
-
-  // First, the Router needs permission to spend the user's shares
-  const {
-    state: permitState,
-    action: permitAction,
-    result: permitResult,
-  } = usePermit(chainId, lender, owner, ALOE_II_ROUTER_ADDRESS[chainId], shares.toString(), shouldUseChecks);
-  const deadline = BigNumber.from(permitResult.deadline);
-  const signature = permitResult.signature ?? ({ v: 0, r: '0x', s: '0x' } as Signature);
-
-  // Next, we set up the actual call
-  const { config: configRedeemWithChecks } = usePrepareContractWrite({
-    ...router,
-    functionName: 'redeemWithChecks',
-    args: [lender, shares, deadline, signature.v, signature.r as `0x${string}`, signature.s as `0x${string}`],
-    enabled: shouldUseChecks && shares.gt(0) && permitResult.signature !== undefined,
-  });
-  const {
-    write: redeemWithChecks,
-    data: redeemWithChecksTxn,
-    isLoading: isAskingUserToRedeemWithChecks,
-    reset: resetRedeemWithChecksTxn,
-  } = useContractWrite({
-    ...configRedeemWithChecks,
-    request: configRedeemWithChecks.request
-      ? {
-          ...configRedeemWithChecks.request,
-          gasLimit: configRedeemWithChecks.request.gasLimit.mul(GAS_LIMIT_CUSHION).div(100),
-        }
-      : undefined,
-  });
-
   let state: RedeemState;
   let action: (() => void) | undefined;
 
@@ -145,24 +100,20 @@ export function useRedeem(chainId: number, lender: Address, amount: GN, owner: A
   } else if (isFetchingMaxData || isFetchingShares) {
     state = RedeemState.FETCHING_DATA;
     action = undefined;
-  } else if (isAskingUserToRedeem || isAskingUserToRedeemWithChecks) {
+  } else if (isAskingUserToRedeem) {
     state = RedeemState.ASKING_USER_TO_REDEEM;
     action = undefined;
-  } else if (!shouldUseChecks) {
+  } else {
     state = RedeemState.READY_TO_REDEEM;
     action = redeem;
-  } else {
-    state = PERMIT_STATE_TO_REDEEM_STATE[permitState] ?? RedeemState.READY_TO_REDEEM;
-    action = permitAction ?? redeemWithChecks;
   }
 
   return {
     state,
     action,
-    txn: redeemTxn ?? redeemWithChecksTxn,
+    txn: redeemTxn,
     resetTxn() {
       resetRedeemTxn();
-      resetRedeemWithChecksTxn();
     },
     maxAmount,
   };
