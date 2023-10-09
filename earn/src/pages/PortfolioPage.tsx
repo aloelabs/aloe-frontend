66e7fceb9e13dfe3980cd5aa36ca8cf6d6c595a2
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-10-08 17:00:32 -0700

    Removing console log

--- earn/src/pages/PortfolioPage.tsx
+++ earn/src/pages/PortfolioPage.tsx
@@ -213,7 +213,6 @@ export default function PortfolioPage() {
     (async () => {
       const chainId = (await provider.getNetwork()).chainId;
       const results = await getAvailableLendingPairs(chainId, provider);
-      console.log(results);
       setLendingPairs(results);
       setIsLoading(false);
     })();
