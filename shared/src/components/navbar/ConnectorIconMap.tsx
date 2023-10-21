2e7d3d6738fe525636e391fd358deb722718cc7f
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-10-21 10:13:50 -0700

    Adding proper rabby integration

--- shared/src/components/navbar/ConnectorIconMap.tsx
+++ shared/src/components/navbar/ConnectorIconMap.tsx
@@ -1,6 +1,7 @@
-import CoinbaseWalletIcon from '../../assets/svg/CoinbaseWalletLogo';
-import MetaMaskIcon from '../../assets/svg/MetaMaskLogo';
-import WalletConnectIcon from '../../assets/svg/WalletConnectLogo';
+import CoinbaseWalletIcon from '../../assets/svg/connectors/CoinbaseWalletLogo';
+import MetaMaskIcon from '../../assets/svg/connectors/MetaMaskLogo';
+import WalletConnectIcon from '../../assets/svg/connectors/WalletConnectLogo';
+import RabbyIcon from '../../assets/svg/connectors/RabbyLogo';
 
 export function getIconForWagmiConnectorNamed(name: string): JSX.Element {
   switch (name) {
@@ -8,7 +9,10 @@ export function getIconForWagmiConnectorNamed(name: string): JSX.Element {
       return <WalletConnectIcon width={40} height={40} />;
     case 'Coinbase Wallet':
       return <CoinbaseWalletIcon width={40} height={40} />;
-    case 'Injected':
+    case 'MetaMask':
+      return <MetaMaskIcon width={40} height={40} />;
+    case 'Rabby':
+      return <RabbyIcon width={40} height={40} />;
     default:
       return <MetaMaskIcon width={40} height={40} />;
   }
