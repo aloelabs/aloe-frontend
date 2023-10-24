56e4da46a4a60fef49c5a9bbe7b53ffe0b627465
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-10-23 21:21:11 -0700

    Adding notification service link (#684)

--- shared/src/data/constants/Values.ts
+++ shared/src/data/constants/Values.ts
@@ -4,3 +4,4 @@ export const DEFAULT_CHAIN = optimism;
 export const DEFAULT_ETHERSCAN_URL = 'https://etherscan.io';
 export const Q32 = 0x100000000;
 export const API_GEO_FENCING_URL = 'https://geo-fencing.aloe.capital/v1/verify';
+export const NOTIFICATION_BOT_URL = 'https://t.me/aloe_notifier_bot';
