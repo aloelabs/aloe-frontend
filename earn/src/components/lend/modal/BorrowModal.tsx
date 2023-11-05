021a426a1dc7d87951b12786afbf931da621e0f9
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-11-04 20:36:01 -0700

    Adding max borrows

--- earn/src/components/lend/modal/BorrowModal.tsx
+++ earn/src/components/lend/modal/BorrowModal.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 
+import { BigNumber } from 'ethers';
+import { volatilityOracleAbi } from 'shared/lib/abis/VolatilityOracle';
 import { FilledGradientButton } from 'shared/lib/components/common/Buttons';
+import { SquareInputWithMax } from 'shared/lib/components/common/Input';
 import Modal from 'shared/lib/components/common/Modal';
 import TokenAmountInput from 'shared/lib/components/common/TokenAmountInput';
 import { Text } from 'shared/lib/components/common/Typography';
+import { ALOE_II_ORACLE_ADDRESS } from 'shared/lib/data/constants/ChainSpecific';
+import { Q32 } from 'shared/lib/data/constants/Values';
 import { GN, GNFormat } from 'shared/lib/data/GoodNumber';
 import { formatNumberInput } from 'shared/lib/util/Numbers';
+import { useContractRead } from 'wagmi';
 
+import { ChainContext } from '../../../App';
+import { ALOE_II_LIQUIDATION_INCENTIVE, ALOE_II_MAX_LEVERAGE } from '../../../data/constants/Values';
 import { BorrowEntry, CollateralEntry } from '../BorrowingWidget';
-
-// const SECONDARY_COLOR = 'rgba(130, 160, 182, 1)';
 
 enum ConfirmButtonState {
   WAITING_FOR_USER,
@@ -46,28 +52,62 @@ export default function BorrowModal(props: BorrowModalProps) {
   const { isOpen, selectedBorrows, selectedCollateral, setIsOpen } = props;
   const [collateralAmountStr, setCollateralAmountStr] = useState<string>('');
   const [borrowAmountStr, setBorrowAmountStr] = useState<string>('');
-  // const { activeChain } = useContext(ChainContext);
-  // const { address: userAddress } = useAccount();
+  const { activeChain } = useContext(ChainContext);
 
   const selectedBorrow = selectedBorrows.find(
     (borrow) => borrow.collateral.address === selectedCollateral.asset.address
   );
 
-  // const selectedLendingPair = selectedCollateral.matchingPairs.find(
-  //   (pair) =>
-  //     pair.token0.address === selectedBorrow?.asset.address || pair.token1.address === selectedBorrow?.asset.address
-  // );
+  const selectedLendingPair = selectedCollateral.matchingPairs.find(
+    (pair) =>
+      pair.token0.address === selectedBorrow?.asset.address || pair.token1.address === selectedBorrow?.asset.address
+  );
 
-  // const { data: consultData } = useContractRead({
-  //   abi: volatilityOracleAbi,
-  //   address: ALOE_II_ORACLE_ADDRESS[activeChain.id],
-  //   args: [selectedLendingPair?.uniswapPool || '0x', Q32],
-  //   functionName: 'consult',
-  //   enabled: selectedLendingPair !== undefined,
-  // });
+  const { data: consultData } = useContractRead({
+    abi: volatilityOracleAbi,
+    address: ALOE_II_ORACLE_ADDRESS[activeChain.id],
+    args: [selectedLendingPair?.uniswapPool || '0x', Q32],
+    functionName: 'consult',
+    enabled: selectedLendingPair !== undefined,
+  });
 
   const userBalance = GN.fromNumber(selectedCollateral.balance, selectedCollateral.asset.decimals);
   const collateralAmount = GN.fromDecimalString(collateralAmountStr || '0', selectedCollateral.asset.decimals);
+  const borrowAmount = GN.fromDecimalString(borrowAmountStr || '0', selectedBorrow?.asset.decimals ?? 0);
+
+  const maxBorrowAmount = useMemo(() => {
+    if (consultData === undefined || selectedBorrow === undefined) {
+      return null;
+    }
+    const sqrtPriceX96 = GN.fromBigNumber(consultData?.[1] ?? BigNumber.from('0'), 96, 2);
+    const nSigma = selectedLendingPair?.nSigma ?? 0;
+    const iv = consultData[2].div(1e6).toNumber() / 1e6;
+    let ltv = 1 / ((1 + 1 / ALOE_II_MAX_LEVERAGE + 1 / ALOE_II_LIQUIDATION_INCENTIVE) * Math.exp(nSigma * iv));
+    ltv = Math.max(0.1, Math.min(ltv, 0.9));
+
+    let inTermsOfBorrow = collateralAmount;
+    if (selectedLendingPair?.token0.address === selectedCollateral.asset.address) {
+      inTermsOfBorrow = inTermsOfBorrow
+        .mul(sqrtPriceX96)
+        .mul(sqrtPriceX96)
+        .setResolution(selectedBorrow.asset.decimals);
+    } else {
+      inTermsOfBorrow = inTermsOfBorrow
+        .div(sqrtPriceX96)
+        .div(sqrtPriceX96)
+        .setResolution(selectedBorrow.asset.decimals);
+    }
+    const maxBorrowSupply = GN.fromNumber(selectedBorrow.supply, selectedBorrow.asset.decimals);
+    const maxPotentialBorrow = inTermsOfBorrow.recklessMul(ltv);
+    return GN.min(maxPotentialBorrow, maxBorrowSupply).recklessMul(0.8);
+  }, [
+    consultData,
+    selectedBorrow,
+    selectedLendingPair?.nSigma,
+    selectedLendingPair?.token0.address,
+    collateralAmount,
+    selectedCollateral.asset.address,
+  ]);
 
   let confirmButtonState: ConfirmButtonState;
   if (collateralAmount.gt(userBalance)) {
@@ -106,20 +146,35 @@ export default function BorrowModal(props: BorrowModalProps) {
           <Text size='M' weight='bold'>
             Borrow
           </Text>
-          <TokenAmountInput
-            token={selectedBorrow.asset}
-            value={borrowAmountStr}
-            onChange={(value) => {
-              const output = formatNumberInput(value);
-              if (output != null) {
-                setBorrowAmountStr(output);
-              }
-            }}
-          />
-          <FilledGradientButton size='M' fillWidth={true} disabled={!confirmButton.enabled}>
-            {confirmButton.text}
-          </FilledGradientButton>
+          <div>
+            <Text size='M' className='mb-2'>
+              {selectedBorrow.asset.symbol}
+            </Text>
+            <SquareInputWithMax
+              size='L'
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                const output = formatNumberInput(event.target.value);
+                if (output != null) {
+                  setBorrowAmountStr(output);
+                }
+              }}
+              value={borrowAmountStr}
+              onMaxClick={() => {
+                if (maxBorrowAmount) {
+                  setBorrowAmountStr(maxBorrowAmount.toString(GNFormat.DECIMAL));
+                }
+              }}
+              maxDisabled={maxBorrowAmount === null || borrowAmount.eq(maxBorrowAmount)}
+              maxButtonText='80% Max'
+              placeholder='0.00'
+              fullWidth={true}
+              inputClassName={borrowAmountStr !== '' ? 'active' : ''}
+            />
+          </div>
         </div>
+        <FilledGradientButton size='M' fillWidth={true} disabled={!confirmButton.enabled}>
+          {confirmButton.text}
+        </FilledGradientButton>
       </div>
     </Modal>
   );
