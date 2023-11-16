800bf5f900485ea3424c51bc1249c6e4881da1bf
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-11-15 22:43:08 -0800

    Adding sorting to supply table (#719)

--- earn/src/components/lend/SupplyTable.tsx
+++ earn/src/components/lend/SupplyTable.tsx
@@ -1,11 +1,14 @@
 import { useMemo, useState } from 'react';
 
 import { SendTransactionResult } from '@wagmi/core';
+import DownArrow from 'shared/lib/assets/svg/DownArrow';
+import UpArrow from 'shared/lib/assets/svg/UpArrow';
 import { FilledGreyButton } from 'shared/lib/components/common/Buttons';
 import Pagination from 'shared/lib/components/common/Pagination';
 import TokenIcon from 'shared/lib/components/common/TokenIcon';
 import { Text, Display } from 'shared/lib/components/common/Typography';
 import { GREY_600 } from 'shared/lib/data/constants/Colors';
+import useSortableData from 'shared/lib/data/hooks/UseSortableData';
 import { Kitty } from 'shared/lib/data/Kitty';
 import { Token } from 'shared/lib/data/Token';
 import { formatTokenAmount } from 'shared/lib/util/Numbers';
@@ -41,6 +44,49 @@ const HoverableRow = styled.tr`
   }
 `;
 
+const SortButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+const StyledDownArrow = styled(DownArrow)`
+  width: 12px;
+  height: 12px;
+
+  path {
+    stroke: rgba(130, 160, 182, 1);
+    stroke-width: 3px;
+  }
+`;
+
+const StyledUpArrow = styled(UpArrow)`
+  width: 12px;
+  height: 12px;
+
+  path {
+    stroke: rgba(130, 160, 182, 1);
+    stroke-width: 3px;
+  }
+`;
+
+type SortArrowProps = {
+  isSorted: boolean;
+  isSortedDesc: boolean;
+};
+
+function SortArrow(props: SortArrowProps) {
+  const { isSorted, isSortedDesc } = props;
+  if (!isSorted) {
+    return null;
+  }
+  if (isSortedDesc) {
+    return <StyledDownArrow />;
+  } else {
+    return <StyledUpArrow />;
+  }
+}
+
 export type SupplyTableRow = {
   asset: Token;
   kitty: Kitty;
@@ -63,16 +109,21 @@ export default function SupplyTable(props: SupplyTableProps) {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedSupply, setSelectedSupply] = useState<SupplyTableRow | null>(null);
   const [selectedWithdraw, setSelectedWithdraw] = useState<SupplyTableRow | null>(null);
+  const { sortedRows, requestSort, sortConfig } = useSortableData(rows, {
+    primaryKey: 'suppliedBalanceUsd',
+    secondaryKey: 'suppliableBalanceUsd',
+    direction: 'descending',
+  });
 
   const { address: userAddress } = useAccount();
 
   const pages: SupplyTableRow[][] = useMemo(() => {
     const pages: SupplyTableRow[][] = [];
-    for (let i = 0; i < rows.length; i += PAGE_SIZE) {
-      pages.push(rows.slice(i, i + PAGE_SIZE));
+    for (let i = 0; i < sortedRows.length; i += PAGE_SIZE) {
+      pages.push(sortedRows.slice(i, i + PAGE_SIZE));
     }
     return pages;
-  }, [rows]);
+  }, [sortedRows]);
   if (pages.length === 0) {
     return null;
   }
@@ -93,19 +144,37 @@ export default function SupplyTable(props: SupplyTableProps) {
                 </Text>
               </th>
               <th className='px-4 py-2 text-end whitespace-nowrap'>
-                <Text size='M' weight='bold'>
-                  APY
-                </Text>
+                <SortButton onClick={() => requestSort('apy')}>
+                  <SortArrow
+                    isSorted={sortConfig?.primaryKey === 'apy' ?? false}
+                    isSortedDesc={sortConfig?.direction === 'descending' ?? false}
+                  />
+                  <Text size='M' weight='bold'>
+                    APY
+                  </Text>
+                </SortButton>
               </th>
               <th className='px-4 py-2 text-end whitespace-nowrap'>
-                <Text size='M' weight='bold'>
-                  Wallet Balance
-                </Text>
+                <SortButton onClick={() => requestSort('suppliableBalanceUsd', 'suppliedBalanceUsd')}>
+                  <SortArrow
+                    isSorted={sortConfig?.primaryKey === 'suppliableBalanceUsd' || false}
+                    isSortedDesc={sortConfig?.direction === 'descending' || false}
+                  />
+                  <Text size='M' weight='bold'>
+                    Wallet Balance
+                  </Text>
+                </SortButton>
               </th>
               <th className='px-4 py-2 text-end whitespace-nowrap'>
-                <Text size='M' weight='bold'>
-                  Aloe Balance
-                </Text>
+                <SortButton onClick={() => requestSort('suppliedBalanceUsd', 'suppliableBalanceUsd')}>
+                  <SortArrow
+                    isSorted={sortConfig?.primaryKey === 'suppliedBalanceUsd' ?? false}
+                    isSortedDesc={sortConfig?.direction === 'descending' ?? false}
+                  />
+                  <Text size='M' weight='bold'>
+                    Aloe Balance
+                  </Text>
+                </SortButton>
               </th>
               {/* <th className='px-4 py-2 text-center whitespace-nowrap'>
                 <Text size='M' weight='bold'>
