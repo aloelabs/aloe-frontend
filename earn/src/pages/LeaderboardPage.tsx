35bf33678c866b62c6bdd3c0367d608bc23d1381
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-12-16 20:26:33 -0600

    Adding leaderboard page for each chain

--- earn/src/pages/LeaderboardPage.tsx
+++ earn/src/pages/LeaderboardPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 
 import axios, { AxiosResponse } from 'axios';
 import { BigNumber } from 'ethers';
@@ -10,6 +10,7 @@ import { GN, GNFormat } from 'shared/lib/data/GoodNumber';
 import useSafeState from 'shared/lib/data/hooks/UseSafeState';
 import styled from 'styled-components';
 
+import { ChainContext } from '../App';
 import { API_LEADERBOARD_URL } from '../data/constants/Values';
 import { LeaderboardResponseEntry } from '../data/LeaderboardResponse';
 
@@ -55,22 +56,24 @@ const TableBodyCell = styled.td`
 export default function LeaderboardPage() {
   const [currentPage, setCurrentPage] = useState(1);
   const [leaderboardEntries, setLeaderboardEntries] = useSafeState<Array<LeaderboardEntry> | null>(null);
+  const { activeChain } = useContext(ChainContext);
 
   useEffect(() => {
     (async () => {
       let leaderboardResponse: AxiosResponse<Array<LeaderboardResponseEntry>>;
       try {
-        leaderboardResponse = await axios.get(API_LEADERBOARD_URL);
+        leaderboardResponse = await axios.get(`${API_LEADERBOARD_URL}?chainId=${activeChain.id}`);
       } catch (e) {
         return;
       }
+      if (!leaderboardResponse.data) return;
       const updatedLeaderboardEntries = leaderboardResponse.data.map((entry) => ({
         address: entry.address,
         score: GN.fromBigNumber(BigNumber.from(entry.score), 18),
       }));
       setLeaderboardEntries(updatedLeaderboardEntries);
     })();
-  }, [setLeaderboardEntries]);
+  }, [activeChain.id, setLeaderboardEntries]);
 
   const pages: LeaderboardEntry[][] = useMemo(() => {
     if (leaderboardEntries == null) {
@@ -83,67 +86,65 @@ export default function LeaderboardPage() {
     return pages;
   }, [leaderboardEntries]);
 
-  if (pages.length === 0) {
-    return null;
-  }
-
   return (
     <AppPage>
       <div className='flex flex-col gap-4 max-w-screen-xl m-auto'>
         <Text size='XXL' className='mb-4'>
           Aloe Leaderboard
         </Text>
-        <TableContainer>
-          <Table>
-            <TableHeader>
-              <tr>
-                <TableHeaderCell>
-                  <Text size='M' weight='bold'>
-                    Rank
-                  </Text>
-                </TableHeaderCell>
-                <TableHeaderCell>
-                  <Text size='M' weight='bold'>
-                    Address
-                  </Text>
-                </TableHeaderCell>
-                <TableHeaderCell>
-                  <Text size='M' weight='bold'>
-                    Rewards
-                  </Text>
-                </TableHeaderCell>
-              </tr>
-            </TableHeader>
-            <TableBody>
-              {pages[currentPage - 1]?.map((entry, index) => (
+        {pages.length > 0 && (
+          <TableContainer>
+            <Table>
+              <TableHeader>
                 <tr>
-                  <TableBodyCell>
-                    <Text size='M'>{(currentPage - 1) * PAGE_SIZE + index + 1}</Text>
-                  </TableBodyCell>
-                  <TableBodyCell>
-                    <Text size='M'>{entry.address}</Text>
-                  </TableBodyCell>
-                  <TableBodyCell>
-                    <Display size='XS'>{entry.score.toString(GNFormat.LOSSY_HUMAN)}</Display>
-                  </TableBodyCell>
+                  <TableHeaderCell>
+                    <Text size='M' weight='bold'>
+                      Rank
+                    </Text>
+                  </TableHeaderCell>
+                  <TableHeaderCell>
+                    <Text size='M' weight='bold'>
+                      Address
+                    </Text>
+                  </TableHeaderCell>
+                  <TableHeaderCell>
+                    <Text size='M' weight='bold'>
+                      Rewards
+                    </Text>
+                  </TableHeaderCell>
                 </tr>
-              ))}
-            </TableBody>
-            <tfoot>
-              <tr>
-                <td className='px-4 py-2' colSpan={5}>
-                  <Pagination
-                    currentPage={currentPage}
-                    itemsPerPage={PAGE_SIZE}
-                    totalItems={leaderboardEntries?.length ?? 0}
-                    loading={false}
-                    onPageChange={(page) => setCurrentPage(page)}
-                  />
-                </td>
-              </tr>
-            </tfoot>
-          </Table>
-        </TableContainer>
+              </TableHeader>
+              <TableBody>
+                {pages[currentPage - 1]?.map((entry, index) => (
+                  <tr key={entry.address}>
+                    <TableBodyCell>
+                      <Text size='M'>{(currentPage - 1) * PAGE_SIZE + index + 1}</Text>
+                    </TableBodyCell>
+                    <TableBodyCell>
+                      <Text size='M'>{entry.address}</Text>
+                    </TableBodyCell>
+                    <TableBodyCell>
+                      <Display size='XS'>{entry.score.toString(GNFormat.LOSSY_HUMAN)}</Display>
+                    </TableBodyCell>
+                  </tr>
+                ))}
+              </TableBody>
+              <tfoot>
+                <tr>
+                  <td className='px-4 py-2' colSpan={5}>
+                    <Pagination
+                      currentPage={currentPage}
+                      itemsPerPage={PAGE_SIZE}
+                      totalItems={leaderboardEntries?.length ?? 0}
+                      loading={false}
+                      onPageChange={(page) => setCurrentPage(page)}
+                    />
+                  </td>
+                </tr>
+              </tfoot>
+            </Table>
+          </TableContainer>
+        )}
       </div>
     </AppPage>
   );
